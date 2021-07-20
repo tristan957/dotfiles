6bc7890bf22928a4c7e2ee22a2ce1980660a94b2
Tristan Partin <tristan@partin.io>
2021-07-20 10:51:30 -0500

    Remove plug statements in init.lua

--- nvim/.config/nvim/lua/tristan957/init.lua
+++ nvim/.config/nvim/lua/tristan957/init.lua
@@ -1,15 +1,3 @@
-vim.cmd(
-	[[
-		Plug 'ThePrimeagen/git-worktree.nvim'
-		Plug 'nvim-lua/popup.nvim'
-		Plug 'nvim-lua/plenary.nvim'
-		Plug 'nvim-telescope/telescope.nvim'
-		Plug 'nvim-treesitter/nvim-treesitter', { 'do': ':TSUpdate' }
-		Plug 'neovim/nvim-lspconfig'
-		Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
-	]]
-)
-
 require('tristan957.tree-sitter')
 require('tristan957.lsp')
 require('tristan957.telescope')
