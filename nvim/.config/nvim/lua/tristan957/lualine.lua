ab7e150baaf3d372b96428c12433dcdd79c5398c
Tristan Partin <tristan@partin.io>
2021-07-28 22:54:53 -0500

    Format lua

--- nvim/.config/nvim/lua/tristan957/lualine.lua
+++ nvim/.config/nvim/lua/tristan957/lualine.lua
@@ -2,7 +2,7 @@ require("lualine").setup({
 	options = {
 		icons_enabled = false,
 		theme = "powerline",
-		component_separators = {"|", "|"},
+		component_separators = { "|", "|" },
 		section_separators = "",
 	},
 	sections = {
