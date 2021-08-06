af6d1defea1eecb2715d653b098a9d6d3886e105
Tristan Partin <tristan@partin.io>
2021-08-06 02:42:59 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -u
+set -o pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+if ! type stow > /dev/null 2>&1; then
+	echo "stow is not installed" >&2
+	exit 1
+fi
+
+packages=()
+dry_run=0
+
+while getopts ":dp:" o; do
+	case $o in
+		d) dry_run=1;;
+		p) packages+=("$OPTARG");;
+		:) echo "'$1' requires an argument" >&2; exit 1;;
+		?) echo "Unknown flag '$1'" >&2; exit 1;;
+	esac
+done
+
+for p in "${packages[@]}"; do
+	if [ $dry_run -eq 1 ]; then
+		echo Installing "$p" "(dry run)"
+		stow --no --dir "$SCRIPT_DIR" --restow "$p"
+	else
+		echo Installing "$p"
+		stow --dir "$SCRIPT_DIR" --restow "$p"
+	fi
+	status=$?
+	if [ $status -ne 0 ]; then
+		exit $?
+	fi
+done
