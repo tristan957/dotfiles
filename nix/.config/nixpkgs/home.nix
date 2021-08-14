010241b70c102fb4df9989c0acf2a783e63cc060
Tristan Partin <tristan@partin.io>
2021-08-14 14:07:44 -0500

    Add a basic home.nix
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ nix/.config/nixpkgs/home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "tristan957";
+  home.homeDirectory = "/home/tristan957";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+  home.packages = [
+    pkgs.neovim-nightly
+	pkgs.nodePackages.bash-language-server
+	pkgs.gopls
+	pkgs.sumneko-lua-language-server
+  ];
+}
