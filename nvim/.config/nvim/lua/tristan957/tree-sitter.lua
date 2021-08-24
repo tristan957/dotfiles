6b911056e180328e125cb8cc606b3c670c4f83e5
Tristan Partin <tristan@partin.io>
2021-08-23 19:13:28 -0500

    Add ts incremental selection

--- nvim/.config/nvim/lua/tristan957/tree-sitter.lua
+++ nvim/.config/nvim/lua/tristan957/tree-sitter.lua
@@ -7,6 +7,15 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 		additional_vim_regex_highlighting = false,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
 })
 
 vim.cmd([[
