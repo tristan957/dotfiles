b905e70358faee72a64b8c7ac8ec590d81885b04
Tristan Partin <tristan@partin.io>
2022-11-17 14:59:08 -0600

    More spaces

--- nvim/.config/nvim/lua/tristan957/plugins/onedarkpro/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/onedarkpro/init.lua
@@ -1,13 +1,13 @@
 require("onedarkpro").setup({
-	dark_theme = "onedark_vivid",
-	options = {
-		bold = false,
-		cursorline = true,
-		italic = false,
-		underline = false,
-		undercurl = false,
-	},
-	highlights = {
-		["@property.meson"] = { fg = "${white}" },
-	},
+  dark_theme = "onedark_vivid",
+  options = {
+    bold = false,
+    cursorline = true,
+    italic = false,
+    underline = false,
+    undercurl = false,
+  },
+  highlights = {
+    ["@property.meson"] = { fg = "${white}" },
+  },
 })
