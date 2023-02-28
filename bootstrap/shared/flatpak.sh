8ca333fc9d734fadd966dde58eb84c12a027167c
Tristan Partin <tristan@partin.io>
2023-02-27 18:58:02 -0600

    Add flatpak support to bootstrap script

---
+++ bootstrap/shared/flatpak.sh
@@ -0,0 +1,31 @@
+function flatpak_add_remotes() {
+    flatpak remote-add --if-not-exists --user gnome-nightly \
+        https://nightly.gnome.org/gnome-nightly.flatpakrepo
+}
+
+function flatpak_install_apps() {
+    # Flathub
+    flatpak install --user flathub \
+        ch.protonmail.protonmail-bridge \
+        com.belmoussaoui.Obfuscate \
+        com.bitwarden.desktop \
+        com.discordapp.Discord \
+        com.github.finefindus.eyedropper \
+        com.mojang.Minecraft \
+        com.rafaelmardojai.SharePreview \
+        com.slack.Slack \
+        com.spotify.Client \
+        com.valvesoftware.Steam \
+        de.haeckerfelix.Fragments \
+        io.github.realmazharhussain.GdmSettings \
+        org.gnome.design.Contrast \
+        org.gnome.design.IconLibrary \
+        org.gnome.design.Lorem \
+        org.gnome.design.SymbolicPreview \
+        us.zoom.Zoom
+
+    # GNOME Nightly
+    flatpak install --user gnome-nightly \
+        org.gnome.Adwaita1.Demo \
+        org.gnome.Builder.Devel
+}
