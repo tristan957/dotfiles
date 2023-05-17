dad3a3930fc44a637ae4ced3568ff3c5006d4b7f
Tristan Partin <tristan@partin.io>
2023-05-17 14:17:55 -0500

    Add 1password to the bootstrap

--- bootstrap/fedora/dnf.sh
+++ bootstrap/fedora/dnf.sh
@@ -1,6 +1,10 @@
 dir=$(dirname "${BASH_SOURCE[0]}")
 
 function dnf_add_repos() {
+    # 1Password
+    sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+    sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+
     # Hashicorp
     sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 
