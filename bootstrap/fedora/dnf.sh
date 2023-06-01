4e9059790c5473a953de251e44422fe0795e03c6
Tristan Partin <tristan@partin.io>
2023-06-01 14:47:13 -0500

    Add -y to some dnf commands

--- bootstrap/fedora/dnf.sh
+++ bootstrap/fedora/dnf.sh
@@ -28,12 +28,12 @@ function dnf_install_packages() {
     xargs sudo dnf install -y < "$dir/packages.txt"
 
     # Proprietary codec bs
-    sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} \
+    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} \
         gstreamer1-plugin-openh264 \
         gstreamer1-libav \
         --exclude=gstreamer1-plugins-bad-free-devel
-    sudo dnf install lame\* --exclude=lame-devel
-    sudo dnf group upgrade --with-optional Multimedia
+    sudo dnf install -y lame\* --exclude=lame-devel
+    sudo dnf group upgrade -y --with-optional Multimedia
 }
 
 function dnf_setup() {
