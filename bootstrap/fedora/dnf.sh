c908bdfbcba25dae7a57edebe3137e90579ce146
Tristan Partin <tristan@partin.io>
2023-08-16 14:12:32 -0500

    Alphabetize dnf repos

--- bootstrap/fedora/dnf.sh
+++ bootstrap/fedora/dnf.sh
@@ -8,8 +8,8 @@ function dnf_add_repos() {
     # Hashicorp
     sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 
-    # Swift-MesonLSP
-    sudo dnf copr enable -y jcwasmx86/Swift-MesonLSP
+    # Microsoft
+    curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 
     # RPM Fusion
     sudo dnf install -y \
@@ -17,15 +17,15 @@ function dnf_add_repos() {
     sudo dnf install -y \
         "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
+    # Swift-MesonLSP
+    sudo dnf copr enable -y jcwasmx86/Swift-MesonLSP
+
     # Teleport
     sudo dnf config-manager --add-repo https://rpm.releases.teleport.dev/teleport.repo
 
     # VSCode
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-
-    # Microsoft
-    curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 
     sudo dnf group update -y core
 }
