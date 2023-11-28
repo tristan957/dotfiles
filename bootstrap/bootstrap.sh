071ad633cd6bc0d26da8f514188fa13a7f9d46fe
Tristan Partin <tristan@partin.io>
2023-11-28 00:39:52 -0600

    shfmt

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,12 +4,12 @@ set -e
 set -o pipefail
 
 for f in "$HOME/.bash"*; do
-	# if we are a symlink, we most likely already stowed
-	if [ -h "$f" ]; then
-		continue
-	else
-		rm "$f"
-	fi
+    # if we are a symlink, we most likely already stowed
+    if [ -h "$f" ]; then
+        continue
+    else
+        rm "$f"
+    fi
 done
 mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/bash-completion/completions"
 mkdir -p "$HOME/.opt"
