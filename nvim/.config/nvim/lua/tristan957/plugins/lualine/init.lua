018d7f28573815511e62d7197cc1b3ebbea14409
Tristan Partin <tristan@partin.io>
2023-12-26 19:32:33 -0600

    Update lualint config

--- nvim/.config/nvim/lua/tristan957/plugins/lualine/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/lualine/init.lua
@@ -1,10 +1,8 @@
-local extract_highlight_colors = require("lualine.utils.utils").extract_highlight_colors
-
 require("lualine").setup({
   options = {
     icons_enabled = false,
     theme = "onedark_vivid",
-    component_separators = { left = "|", right = "|" },
+    component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
   },
   sections = {
@@ -17,12 +15,6 @@ require("lualine").setup({
       {
         "diagnostics",
         sources = { "nvim_diagnostic" },
-        diagnostics_color = {
-          error = { fg = extract_highlight_colors("DiagnosticError", "fg") },
-          warn = { fg = extract_highlight_colors("DiagnosticWarn", "fg") },
-          info = { fg = extract_highlight_colors("DiagnosticInfo", "fg") },
-          hint = { fg = extract_highlight_colors("DiagnosticHint", "fg") },
-        },
         sections = { "error", "warn", "info", "hint" },
         symbols = { error = "E", warn = "W", info = "I", hint = "H" },
         always_visible = false,
