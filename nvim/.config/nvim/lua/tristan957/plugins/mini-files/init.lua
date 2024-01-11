f5794043d1866d37fa804997b05b23ab18a3977d
Tristan Partin <tristan@partin.io>
2024-01-11 17:49:45 -0600

    Update mini-files config

--- nvim/.config/nvim/lua/tristan957/plugins/mini-files/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/mini-files/init.lua
@@ -1,9 +1,46 @@
-local mini_files = require("mini.files")
+local MiniFiles = require("mini.files")
 
-mini_files.setup({
+MiniFiles.setup({
   use_as_default_explorer = true,
 })
 
-vim.keymap.set("n", "<leader>n", function()
-  mini_files.open(vim.api.nvim_buf_get_name(0))
-end, { noremap = true, silent = true })
+local toggle = function()
+  if not MiniFiles.close() then
+    MiniFiles.open(vim.api.nvim_buf_get_name(0))
+  end
+end
+
+vim.keymap.set("n", "<leader>n", toggle, { silent = true })
+
+local map_split = function(buf_id, lhs, close_on_file, direction)
+  local rhs = function()
+    local new_target_window
+    vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+      vim.cmd(direction .. " split")
+      new_target_window = vim.api.nvim_get_current_win()
+    end)
+
+    MiniFiles.set_target_window(new_target_window)
+    MiniFiles.go_in({ close_on_file = close_on_file })
+  end
+
+  -- Adding `desc` will result into `show_help` entries
+  local desc = "Split " .. direction
+  if close_on_file then
+    desc = desc .. " plus"
+  end
+
+  vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+
+    map_split(buf_id, "gs", false, "belowright horizontal")
+    map_split(buf_id, "gS", true, "belowright horizontal")
+    map_split(buf_id, "gv", false, "belowright vertical")
+    map_split(buf_id, "gV", true, "belowright vertical")
+  end,
+})
