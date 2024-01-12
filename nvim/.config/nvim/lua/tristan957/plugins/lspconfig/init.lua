24a84ab5c843d804f3f9ef3e003014e8c5190616
Tristan Partin <tristan@partin.io>
2024-01-11 21:46:52 -0600

    Update lua_ls config

--- nvim/.config/nvim/lua/tristan957/plugins/lspconfig/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/lspconfig/init.lua
@@ -140,26 +140,26 @@ lspconfig.lua_ls.setup({
   capabilities = capabilities,
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    if
-      not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
-      then
-        client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            workspace = {
-              library = {
-                vim.env.VIMRUNTIME,
-              },
-            },
+    if not vim.loop.fs_stat(path .. "/.luarc.json") then
+      client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+        Lua = {
+          globals = {
+            "vim",
           },
-        })
+          runtime = {
+            version = "LuaJIT",
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+        },
+      })
 
-        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-      end
-  return true
-  end
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+
+    return true
+  end,
 })
 
 lspconfig.marksman.setup({
