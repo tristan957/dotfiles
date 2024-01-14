5d04250de526eb945b92bc1ac84ddbaeba5c735e
Tristan Partin <tristan@partin.io>
2024-01-14 12:09:27 -0600

    Restrict files that scripts find

--- scripts/shfmt.sh
+++ scripts/shfmt.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-files=$(find . -name "*.sh" -type f -not -name "git-prompt.sh")
+files=$(find . -name "*.sh" -type f \
+    -not -name "git-prompt.sh" \
+    -not -path "./cargo/.opt/cargo/*")
 
 # shellcheck disable=SC2086
 shfmt --diff --language-dialect bash --indent 4 programs/.local/bin/gdbwait $files
