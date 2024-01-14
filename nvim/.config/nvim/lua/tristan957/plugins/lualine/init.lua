29eacc1c1e0bb801aeed7355b369391a37eabfe8
Tristan Partin <tristan@partin.io>
2024-01-13 21:27:16 -0600

    Try a different lualine config

--- nvim/.config/nvim/lua/tristan957/plugins/lualine/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/lualine/init.lua
@@ -1,17 +1,19 @@
 ---@type LazySpec
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "onedarkpro.nvim" },
+  dependencies = {
+    "onedarkpro.nvim",
+  },
   event = "UIEnter",
   config = function()
     local lualine = require("lualine")
 
     lualine.setup({
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = "onedark_vivid",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        -- component_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
       },
       sections = {
         lualine_b = {
@@ -24,11 +26,20 @@ return {
             "diagnostics",
             sources = { "nvim_diagnostic" },
             sections = { "error", "warn", "info", "hint" },
-            symbols = { error = "E", warn = "W", info = "I", hint = "H" },
             always_visible = false,
             update_in_insert = true,
           },
         },
+        lualine_x = {
+          "encoding",
+          {
+            "fileformat",
+            icons_enabled = false,
+          },
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
     })
   end,
