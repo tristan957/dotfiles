64efe2b4ea868b2d2ebf7c2d0cc003d5a2360e02
Tristan Partin <tristan@partin.io>
2024-01-17 15:59:15 -0600

    Make a first attempt at removing mini.move

--- nvim/.config/nvim/lua/tristan957/keymaps.lua
+++ nvim/.config/nvim/lua/tristan957/keymaps.lua
@@ -15,6 +15,27 @@ vim.keymap.set(
 )
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
+-- These don't currently work if selection includes the top or bottom lines.
+-- vim.keymap.set("v", "<M-k>", ":m '>+1<CR>gv=gv", { desc = "Move selection up" })
+-- vim.keymap.set("v", "<M-j>", ":m '<-2<CR>gv=gv", { desc = "Move selection down" })
+
+-- This somehow doesn't work great with undo. Indent the feedkeys line in, and
+-- then try to undo. Cursor goes to beginning of line.
+-- local function move_horizontally(cmd)
+--   return function()
+--     local ref_curpos = vim.fn.getcurpos()
+--     local ref_last_col = vim.fn.col("$")
+--
+--     vim.api.nvim_feedkeys(cmd, "xtn", false)
+--
+--     local col_diff = vim.fn.col("$") - ref_last_col
+--     local new_col = math.max(ref_curpos[3] + col_diff, 1)
+--     vim.fn.cursor({ vim.fn.line("."), new_col, ref_curpos[4], ref_curpos[5] + col_diff })
+--   end
+-- end
+-- vim.keymap.set("v", "<M-l>", move_horizontally(">gv"), { desc = "Move selection rightward" })
+-- vim.keymap.set("v", "<M-h>", move_horizontally("<gv"), { desc = "Move selection rightward" })
+
 -- Similar to o or O without entering insert mode
 vim.keymap.set("n", "oo", "o<Esc>", { desc = "Enter newline below cursor" })
 vim.keymap.set("n", "OO", "O<Esc>", { desc = "Enter newline above cursor" })
