e9a8b52cd4a22f6554feaab206dfcc91d56bfc3c
Tristan Partin <tristan@partin.io>
2024-01-17 15:59:39 -0600

    Try out nvim-lsp-notify for a bit

--- nvim/.config/nvim/lua/tristan957/plugins/notify/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/notify/init.lua
@@ -1,14 +1,34 @@
----@type LazySpec
+---@type LazySpec[]
 return {
-  "rcarriga/nvim-notify",
-  event = "UIEnter",
-  config = function()
-    local notify = require("notify")
+  {
+    "rcarriga/nvim-notify",
+    event = "UIEnter",
+    config = function()
+      local notify = require("notify")
 
-    notify.setup({
-      stages = "fade",
-    })
+      notify.setup({
+        stages = "fade",
+      })
 
-    vim.notify = notify
-  end,
+      vim.notify = notify
+
+      -- Future: https://github.com/rcarriga/nvim-notify/wiki/Usage-Recipes
+    end,
+  },
+  {
+    "mrded/nvim-lsp-notify",
+    dependencies = {
+      "rcarriga/nvim-notify",
+    },
+    -- event = "UIEnter",
+    config = function()
+      local lsp_notify = require("lsp-notify")
+
+      -- Because we depend on nvim-notify, vim.notify is a fine default to use.
+      -- nvim-notify will handle everything.
+      lsp_notify.setup({
+        done = ""
+      })
+    end
+  },
 }
