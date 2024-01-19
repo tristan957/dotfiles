16f8181881569abde8be4b1577f58f26f874ef11
Tristan Partin <tristan@partin.io>
2024-01-19 00:51:34 -0600

    Add chcompdb to ruff checking

--- scripts/ruff.sh
+++ scripts/ruff.sh
@@ -3,4 +3,7 @@
 files=$(find . -name "*.py" -type f)
 
 # shellcheck disable=SC2086
-ruff check --diff programs/.local/bin/xdp-file-chooser $files
+ruff check --diff \
+    programs/.local/bin/chcompdb \
+    programs/.local/bin/xdp-file-chooser \
+    $files
