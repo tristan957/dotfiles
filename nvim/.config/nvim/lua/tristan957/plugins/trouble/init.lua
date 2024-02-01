9c4c23f6350b7ea992dda5d6efd4bf711c969870
Tristan Partin <tristan@partin.io>
2024-02-01 01:26:36 -0600

    Update trouble keybind

--- nvim/.config/nvim/lua/tristan957/plugins/trouble/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/trouble/init.lua
@@ -10,10 +10,13 @@ return {
 
     trouble.setup({
       icons = true,
-      indent_lines = false, -- add an indent guide below the fold icons
+      indent_lines = true, -- add an indent guide below the fold icons
+      win_config = {
+        border = "rounded",
+      },
       use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
     })
 
-    vim.keymap.set("n", "<leader>dt", "<cmd>TroubleToggle<CR>", { silent = true })
+    vim.keymap.set("n", "\\t", vim.cmd.TroubleToggle, { silent = true })
   end,
 }
