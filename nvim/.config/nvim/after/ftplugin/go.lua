1dfa32c8d03d44d3228512558e58f4c5536f29b9
Tristan Partin <tristan@partin.io>
2024-02-22 21:18:24 -0600

    Fix some go settings

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -1,6 +1,9 @@
+vim.opt_local.expandtab = false
+vim.opt_local.formatoptions = "tcqjro"
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').goimport()
+    require('go.format').goimport()
   end
 })
