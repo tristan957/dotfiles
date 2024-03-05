d8b6e11ea79bf1d036b60324b12b5163d80f7823
Tristan Partin <tristan@partin.io>
2024-03-04 20:39:04 -0600

    Only enable fzf-native if make is available

--- nvim/.config/nvim/lua/tristan957/plugins/telescope/init.lua
+++ nvim/.config/nvim/lua/tristan957/plugins/telescope/init.lua
@@ -7,6 +7,9 @@ return {
     {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
     },
     "nvim-telescope/telescope-ui-select.nvim",
     "rcarriga/nvim-notify",
