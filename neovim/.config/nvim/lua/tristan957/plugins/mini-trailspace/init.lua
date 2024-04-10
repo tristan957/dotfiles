aa993b0c2f12881a70ca5384f7cf7476ab39d2a1
Tristan Partin <tristan@partin.io>
2024-04-10 00:07:12 -0500

    Add autocmd for not highlighting mini.trailspace

--- neovim/.config/nvim/lua/tristan957/plugins/mini-trailspace/init.lua
+++ neovim/.config/nvim/lua/tristan957/plugins/mini-trailspace/init.lua
@@ -6,5 +6,22 @@ return {
     local MiniTrailspace = require("mini.trailspace")
 
     MiniTrailspace.setup()
+
+    local group = vim.api.nvim_create_augroup("tristan957/mini.trailspace", { clear = true })
+    vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter", "InsertLeave" }, {
+      desc = "Stop highlighting trailing white space",
+      group = group,
+      callback = function(ev)
+        if not vim.bo[ev.buf].modifiable then
+          return
+        end
+
+        local should_disable = string.find(vim.bo[ev.buf].formatoptions, "w") ~= nil
+        vim.b[ev.buf].minitrailspace_disable = should_disable
+        if should_disable then
+          MiniTrailspace.unhighlight()
+        end
+      end,
+    })
   end,
 }
