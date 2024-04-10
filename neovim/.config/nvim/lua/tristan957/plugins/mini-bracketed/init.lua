86284fe3736a8919ebe395fd8e8b247c854452ae
Tristan Partin <tristan@partin.io>
2024-04-10 00:06:07 -0500

    Add mini.bracketed to neovim

---
+++ neovim/.config/nvim/lua/tristan957/plugins/mini-bracketed/init.lua
@@ -0,0 +1,24 @@
+---@type LazySpec
+return {
+  "echasnovski/mini.bracketed",
+  config = function()
+    local MiniBracketed = require("mini.bracketed")
+
+    MiniBracketed.setup({
+      buffer = { suffix = "", options = {} },
+      comment = { suffix = "c", options = {} },
+      conflict = { suffix = "x", options = {} },
+      diagnostic = { suffix = "d", options = {} },
+      file = { suffix = "", options = {} },
+      indent = { suffix = "i", options = {} },
+      jump = { suffix = "", options = {} },
+      location = { suffix = "l", options = {} },
+      oldfile = { suffix = "", options = {} },
+      quickfix = { suffix = "q", options = {} },
+      treesitter = { suffix = "n", options = {} },
+      undo = { suffix = "", options = {} },
+      window = { suffix = "w", options = {} },
+      yank = { suffix = "y", options = {} },
+    })
+  end,
+}
