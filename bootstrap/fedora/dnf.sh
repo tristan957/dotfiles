5e0e10185834e5def8bfa46d3da45c8e9ed90393
Tristan Partin <tristan@partin.io>
2024-04-16 16:57:42 -0500

    Track packages to uninstall in Fedora base install

--- bootstrap/fedora/dnf.sh
+++ bootstrap/fedora/dnf.sh
@@ -43,7 +43,8 @@ gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
 function dnf_install_packages() {
     local dir=$(dirname "${BASH_SOURCE[0]}")
 
-    xargs sudo dnf install -y <"$dir/packages.txt"
+    xargs sudo dnf remove -y <"$dir/packages/remove.txt"
+    xargs sudo dnf install -y <"$dir/packages/install.txt"
     xargs sudo dnf install -y <"$dir/work.txt"
 
     # Proprietary codec bs
