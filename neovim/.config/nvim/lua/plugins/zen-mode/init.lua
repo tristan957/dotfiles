83f4d546ce9aede3cde23cff230d4bf8eb59f364
Tristan Partin <tristan@partin.io>
2024-05-07 19:10:21 -0500

    Change keymap for launching Zen mode

--- neovim/.config/nvim/lua/plugins/zen-mode/init.lua
+++ neovim/.config/nvim/lua/plugins/zen-mode/init.lua
@@ -3,6 +3,6 @@ return {
   "folke/zen-mode.nvim",
   cmd = "ZenMode",
   config = function()
-    vim.keymap.set("n", "Z", vim.cmd.ZenMode, { desc = "Toggle zen mode" })
+    vim.keymap.set("n", "ZM", vim.cmd.ZenMode, { desc = "Toggle zen mode" })
   end
 }
