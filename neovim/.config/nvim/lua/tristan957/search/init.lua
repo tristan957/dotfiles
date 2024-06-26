ba077f59e7915cd5162962d3c407afb86b60edcf
Tristan Partin <tristan@partin.io>
2024-06-26 10:12:20 -0500

    Add keybind for searching within the last selection

--- neovim/.config/nvim/lua/tristan957/search/init.lua
+++ neovim/.config/nvim/lua/tristan957/search/init.lua
@@ -1,1 +1,2 @@
 vim.keymap.set({ "i", "n", "v" }, "<A-U>", vim.cmd.nohlsearch, { desc = "Turn off search highlighting" })
+vim.keymap.set({ "n", "v" }, "<A-/>", "<Esc>/\\%V", { desc = "Search within last selection" })
