1007aa2dfea7b151df1a6139baae37f60b5c1f5a
Tristan Partin <tristan@partin.io>
2024-07-03 16:28:38 -0500

    Remove whitespace manip in gitcommit

--- neovim/.config/nvim/after/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,2 +1,8 @@
 vim.opt_local.expandtab = false
 vim.opt_local.formatoptions = "1jntcql/r"
+
+vim.b.minitrailspace_disable = true
+
+vim.api.nvim_clear_autocmds({
+  group = "tristan957_whitespace",
+})
