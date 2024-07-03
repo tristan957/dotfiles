7286acc4e7a199735d3da86801b31dd702ed8fb7
Tristan Partin <tristan@partin.io>
2024-07-03 16:32:57 -0500

    Add mini.icons support

---
+++ neovim/.config/nvim/lua/plugins/mini-icons/init.lua
@@ -0,0 +1,25 @@
+---@type LazySpec
+return {
+  "echasnovski/mini.icons",
+  lazy = true,
+  specs = {
+    { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+  },
+  config = function()
+    local MiniIcons = require("mini.icons")
+
+    MiniIcons.setup({
+      style = "glyph",
+    })
+  end,
+  init = function()
+    package.preload["nvim-web-devicons"] = function()
+      local MiniIcons = require("mini.icons")
+
+      -- needed since it will be false when loading and mini will fail
+      package.loaded["nvim-web-devicons"] = {}
+      MiniIcons.mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
+  end,
+}
--- neovim/.config/nvim/lua/plugins/web-devicons/init.lua
+++
@@ -1,5 +0,0 @@
----@type LazySpec
-return {
-  "nvim-tree/nvim-web-devicons",
-  lazy = true,
-}
