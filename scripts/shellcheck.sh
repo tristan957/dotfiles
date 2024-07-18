8bb35918f2c6edfa6bb67d97cb2700712590291a
Tristan Partin <tristan@partin.io>
2024-07-18 00:00:18 -0500

    Fix shellcheck script

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 files=$(find . -name "*.sh" -type f \
-    -not -name "git-prompt.sh" \
-    -not -path "./cargo/.opt/cargo/*")
+    -not -path "./rust/.opt/cargo/*")
 
 # shellcheck disable=SC2086
 shellcheck --shell bash --source-path bash -x bash/.bashrc $files
