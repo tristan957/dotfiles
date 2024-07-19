1896d4d8fe4bb4bc1e04a53f376f11bec2d26d8b
Tristan Partin <tristan@partin.io>
2024-07-18 23:04:03 -0500

    Minor fixes to comlink config

--- comlink/.config/comlink/init.lua
+++ comlink/.config/comlink/init.lua
@@ -1,11 +1,12 @@
 local comlink = require("comlink")
 
 local proc = assert(io.popen("op --account my.1password.com read op://Personal/chat.sr.ht/credential", "r"))
-local pass = proc:read("*all")
+local pass = proc:read("*l")
 proc:close()
 
 comlink.connect({
   server = "chat.sr.ht",
+  tls = true,
   user = "tristan957",
   nick = "tristan957",
   real_name = "Tristan Partin",
