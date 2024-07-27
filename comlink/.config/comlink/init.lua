582d829628278aaf9d54d1b9838bb7e53722e570
Tristan Partin <tristan@partin.io>
2024-07-27 14:37:44 -0500

    Update comlink config

--- comlink/.config/comlink/init.lua
+++ comlink/.config/comlink/init.lua
@@ -1,10 +1,12 @@
 local comlink = require("comlink")
 
-local proc = assert(io.popen("op --account my.1password.com read op://Personal/chat.sr.ht/credential", "r"))
+local proc =
+  assert(io.popen("op --account my.1password.com read op://Personal/chat.sr.ht/credential", "r"))
 local password = proc:read("*l")
 proc:close()
 
-comlink.connect({
+---@diagnostic disable-next-line:unused-local
+local client = comlink.connect({
   server = "chat.sr.ht",
   tls = true,
   user = "tristan957",
