d73c69a276e222987ba0c4c5e15483fa7410325b
Tristan Partin <tristan@partin.io>
2024-07-31 20:16:58 -0500

    Detect various files as jsonc

---
+++ neovim/.config/nvim/ftdetect/jsonc.lua
@@ -0,0 +1,8 @@
+vim.filetype.add({
+  filename = {
+    ["devcontainer.json"] = "jsonc",
+    [".vscode/extensions.json"] = "jsonc",
+    [".vscode/settings.json"] = "jsonc",
+    [".vscode/tasks.json"] = "jsonc",
+  },
+})
