5b1ac5a70801f25cd6550009d4705d2c6ede3e92
Tristan Partin <tristan@partin.io>
2024-08-07 18:29:42 -0500

    Revert "Tell terminals when CWD changes in fish"
    This reverts commit 306b4bb56f3f13ff781a626be27a847461e2ff9d.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,10 +2,4 @@ if not status is-interactive
     return
 end
 
-function __fish_update_cwd_osc --on-variable PWD --description 'Notify terminals when $PWD changes'
-    printf \e\]7\;file://%s%s\a $hostname (string escape --style=url -- $PWD)
-end
-
-__fish_update_cwd_osc # Run once because we might have already inherited a PWD from an old
-
 set -U fish_greeting
