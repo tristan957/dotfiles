00cd9213a638ccea3e45625d689505f3278840c6
Tristan Partin <tristan@partin.io>
2024-08-09 09:37:34 -0500

    Add /fmt command

--- comlink/.config/comlink/init.lua
+++ comlink/.config/comlink/init.lua
@@ -14,5 +14,22 @@ comlink.connect({
   password = password,
 })
 
+-- https://modern.ircdocs.horse/formatting
+local function format(cmdline)
+  local channel = comlink.selected_channel()
+  if channel == nil then
+    return
+  end
+
+  local s = string.gsub(cmdline, "%*%*(.+)%*%*", "\x02%1\x02")
+  s = string.gsub(s, "%*(.+)%*", "\x1D%1\x1D")
+  s = string.gsub(s, "~(.+)~", "\x1E%1\x1E")
+  s = string.gsub(s, "_(.+)_", "\x1F%1\x1F")
+
+  channel:send_msg(s)
+end
+
+comlink.add_command("fmt", format)
+
 comlink.bind("ctrl+alt+j", "next-channel")
 comlink.bind("ctrl+alt+k", "prev-channel")
