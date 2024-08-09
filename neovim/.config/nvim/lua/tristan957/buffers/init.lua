f59082ddef1cd060761a1facf87f91bc0e728672
Tristan Partin <tristan@partin.io>
2024-08-09 17:13:55 -0500

    Set filetype if we can match on BufWritePost

--- neovim/.config/nvim/lua/tristan957/buffers/init.lua
+++ neovim/.config/nvim/lua/tristan957/buffers/init.lua
@@ -1,2 +1,15 @@
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = function(ev)
+    if vim.bo[ev.buf].filetype ~= "" then
+      return
+    end
+
+    local ft = vim.filetype.match({ buf = ev.buf })
+    if ft ~= nil then
+      vim.bo[ev.buf].filetype = ft
+    end
+  end,
+})
+
 vim.keymap.set("n", "]b", vim.cmd.bnext, { desc = "Go to next buffer" })
 vim.keymap.set("n", "[b", vim.cmd.bprevious, { desc = "Go to previous buffer" })
