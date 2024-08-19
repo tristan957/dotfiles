d369eafeee9b104d6dac6e8199daa99bda08d008
Tristan Partin <tristan@partin.io>
2024-08-19 00:28:22 -0500

    Save Python histfile in XDG_STATE_HOME/python

--- python/startup.py
+++ python/startup.py
@@ -4,16 +4,11 @@ import atexit
 import os
 import readline
 
-from typing import TYPE_CHECKING
+STATE_DIR = os.getenv("XDG_STATE_HOME", os.path.join(os.getenv("HOME"), ".var"))
+PYTHON_STATE_DIR = os.path.join(STATE_DIR, "python")
+HISTFILE = os.path.join(PYTHON_STATE_DIR, "history")
 
-if TYPE_CHECKING:
-    from typing import Union
-
-CACHE_DIR = os.getenv("XDG_CACHE_HOME", os.path.join(os.path.expanduser("~"), ".cache"))
-PYTHON_CACHE_DIR = os.path.join(CACHE_DIR, "python")
-HISTFILE = os.path.join(PYTHON_CACHE_DIR, "history")
-
-os.makedirs(PYTHON_CACHE_DIR, mode=755, exist_ok=True)
+os.makedirs(PYTHON_STATE_DIR, mode=755, exist_ok=True)
 
 try:
     readline.read_history_file(HISTFILE)
@@ -22,8 +17,9 @@ except FileNotFoundError:
     open(HISTFILE, 'wb').close()
     h_len = 0
 
-def save(prev_h_len: int, histfile: Union[None, bytes, os.PathLike[bytes], str, os.PathLike[str]]):
+def save(prev_h_len: int, histfile: None | bytes | os.PathLike[bytes] | str | os.PathLike[str]):
     new_h_len = readline.get_current_history_length()
     readline.set_history_length(1000)
     readline.append_history_file(new_h_len - prev_h_len, histfile)
+
 atexit.register(save, h_len, HISTFILE)
