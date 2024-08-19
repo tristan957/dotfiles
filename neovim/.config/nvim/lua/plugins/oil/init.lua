02102c11fd334c174d5095632c13d94ff48b5040
Tristan Partin <tristan@partin.io>
2024-08-19 18:48:12 -0500

    Allow Oil to show hidden files
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/oil/init.lua
+++ neovim/.config/nvim/lua/plugins/oil/init.lua
@@ -7,7 +7,11 @@ return {
   config = function()
     local oil = require("oil")
 
-    oil.setup()
+    oil.setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
 
     vim.keymap.set("n", "|", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
