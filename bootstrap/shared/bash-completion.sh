dcc73299f9f6c4d4db7733254f9055e3b492c642
Tristan Partin <tristan@partin.io>
2024-09-15 15:41:15 -0500

    Use new bash-completion pattern for rust executables
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- bootstrap/shared/bash-completion.sh
+++
@@ -1,8 +0,0 @@
-function bash_completion_install() {
-    rustup completions bash cargo >"${XDG_DATA_HOME}/bash-completion/completions/cargo"
-    rustup completions bash rustup >"${XDG_DATA_HOME}/bash-completion/completions/rustup"
-}
-
-function bash_completion_setup() {
-    bash_completion_install
-}
--- bootstrap/shared/rustup.sh
+++ bootstrap/shared/rustup.sh
@@ -2,8 +2,22 @@ function rustup_download() {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 }
 
+function rustup_add_components {
+    rustup component add rust-src
+}
+
+function rustup_bash_completion {
+    systemctl --user enable --now \
+        cargo-bash-completion.path \
+        rustup-bash-completion.path
+
+    systemctl --user start \
+        cargo-bash-completion.service \
+        rustup-bash-completion.service
+}
+
 function rustup_setup() {
     rustup_download
-
-    rustup component add rust-src
+    rustup_add_components
+    rustup_bash_completion
 }
