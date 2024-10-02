a8d49e002ac458a8375aa10e662ce289b3a9dfa4
Tristan Partin <tristan@partin.io>
2024-10-02 00:37:13 -0400

    Account for lspconfig updates

--- neovim/.config/nvim/lua/tristan957/lsp/config/lemminx.lua
+++ neovim/.config/nvim/lua/tristan957/lsp/config/lemminx.lua
@@ -1,5 +1,5 @@
 local capabilities = require("tristan957.lsp").capabilities
-local default_config = require("lspconfig.server_configurations.lemminx").default_config
+local default_config = require("lspconfig.configs.lemminx").default_config
 
 return {
   capabilities = capabilities,
