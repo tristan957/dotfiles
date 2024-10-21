df56a5d4f66cc359be455cfdb795d7962fac6992
Tristan Partin <tristan@partin.io>
2024-10-20 23:38:24 -0500

    Remove unused line
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/tristan957/runtimepaths/work/neon/project.lua
+++ neovim/.config/nvim/lua/tristan957/runtimepaths/work/neon/project.lua
@@ -1,8 +1,5 @@
 local group = vim.api.nvim_create_augroup("tristan957_project", { clear = true })
 
--- We setup our own autocmd
-vim.api.nvim_del_augroup_by_name("tristan957_lsp_format")
-
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = group,
   desc = "Format files on save",
