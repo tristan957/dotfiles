d9884b025af0f688b767a6dbac702934bd43b5f2
Tristan Partin <tristan@partin.io>
2024-10-20 23:38:00 -0500

    Load project settings before any other personal settings
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/tristan957/init.lua
+++ neovim/.config/nvim/lua/tristan957/init.lua
@@ -1,3 +1,14 @@
+-- Always load project settings first
+require("tristan957.project-settings").setup({
+  fs_root = vim.fs.joinpath(vim.env.HOME, "Projects"),
+  rtps_root = vim.fs.joinpath(
+  vim.fn.stdpath("config") --[[@as string]],
+  "lua",
+  "tristan957",
+  "runtimepaths"
+  ),
+})
+
 require("tristan957.buffers")
 require("tristan957.diagnostics")
 require("tristan957.linenumbers")
@@ -6,12 +17,3 @@ require("tristan957.quickfix")
 require("tristan957.search")
 require("tristan957.whitespace")
 require("tristan957.yank")
-require("tristan957.project-settings").setup({
-  fs_root = vim.fs.joinpath(vim.env.HOME, "Projects"),
-  rtps_root = vim.fs.joinpath(
-    vim.fn.stdpath("config") --[[@as string]],
-    "lua",
-    "tristan957",
-    "runtimepaths"
-  ),
-})
