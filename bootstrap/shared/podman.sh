4db14dd5d582286cb20d650d10cd242c2481601e
Tristan Partin <tristan@partin.io>
2024-10-27 16:38:36 -0500

    Silence docker warning from podman

--- bootstrap/shared/podman.sh
+++ bootstrap/shared/podman.sh
@@ -3,6 +3,11 @@ function podman_enable_socket {
     systemct --user enable --now podman.socket
 }
 
+function podman_silence_docker_warning {
+    sudo touch /etc/containers/nodocker
+}
+
 function podman_setup {
     podman_enable_socket
+    podman_silence_docker_warning
 }
