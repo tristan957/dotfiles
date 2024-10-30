d8e00d06397c594332db86560789715cf58160a3
Tristan Partin <tristan@partin.io>
2024-10-30 10:08:18 -0500

    Fix pkl-neovim config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/pkl/init.lua
+++ neovim/.config/nvim/lua/plugins/pkl/init.lua
@@ -1,11 +1,14 @@
 ---@type LazySpec
 return {
   "apple/pkl-neovim",
+  lazy = true,
   ft = "pkl",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
   build = function()
+    require('pkl-neovim.internal').init()
+
     vim.cmd("TSInstall! pkl")
   end,
 }
