08334a81fbd309d7ad2f6059986b51c66d632404
Tristan Partin <tristan@partin.io>
2024-11-05 14:14:07 -0600

    Add workaround for rust_analyzer
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/tristan957/lsp/init.lua
+++ neovim/.config/nvim/lua/tristan957/lsp/init.lua
@@ -90,6 +90,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end,
       })
     end
+
+    -- Workaround for https://github.com/neovim/neovim/issues/30985
+    if client.name == "rust_analyzer" then
+      for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+        local default_diagnostic_handler = vim.lsp.handlers[method]
+        vim.lsp.handlers[method] = function(err, result, context, config)
+          if err ~= nil and err.code == -32802 then
+            return
+          end
+          return default_diagnostic_handler(err, result, context, config)
+        end
+      end
+    end
   end,
 })
 
