89be822bf65d843938bef12023505dae173400da
Tristan Partin <tristan@partin.io>
2024-11-11 21:15:20 -0600

    Remove unnecessary lua cast
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/snacks/init.lua
+++ neovim/.config/nvim/lua/plugins/snacks/init.lua
@@ -40,7 +40,7 @@ return {
       ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
       callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        local value = ev.data.params.value
         if not client or type(value) ~= "table" then
           return
         end
