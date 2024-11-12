b6bb4fde89cc4cb010350d0cece5786c4d29adef
Tristan Partin <tristan@partin.io>
2024-11-11 21:11:32 -0600

    Remove code which doesn't exist anymore
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/after/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -2,7 +2,3 @@ vim.opt_local.expandtab = false
 vim.opt_local.formatoptions = "1jntcql/r"
 
 vim.b.minitrailspace_disable = true
-
-vim.api.nvim_clear_autocmds({
-  group = "tristan957_whitespace",
-})
