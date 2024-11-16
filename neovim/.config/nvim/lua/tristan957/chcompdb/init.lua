b80ee96aa00c7ceff755fbfddb1e54174334637f
Tristan Partin <tristan@partin.io>
2024-11-15 21:39:19 -0600

    Add Chcompdb user command
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ neovim/.config/nvim/lua/tristan957/chcompdb/init.lua
@@ -0,0 +1,20 @@
+local function chcompdb(opts)
+  vim.system({ "chcompdb", "--quiet", opts.fargs[1] }, {}, function(o)
+    if o.code ~= 0 then
+      vim.notify(o.stderr, vim.log.levels.ERROR)
+      return
+    end
+
+    vim.notify("Changed the active compilation database to " .. opts.fargs[1], vim.log.levels.INFO)
+  end)
+end
+
+vim.api.nvim_create_autocmd("Filetype", {
+  pattern = { "c", "cpp" },
+  callback = function(ev)
+    vim.api.nvim_buf_create_user_command(ev.buf, "Chcompdb", chcompdb, {
+      nargs = 1,
+      complete = "file",
+    })
+  end,
+})
