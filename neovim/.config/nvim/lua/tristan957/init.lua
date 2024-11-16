a06e2409db82707790df1785c564bdb0d77c2cf4
Tristan Partin <tristan@partin.io>
2024-11-15 21:37:45 -0600

    Format snippet of lua code

--- neovim/.config/nvim/lua/tristan957/init.lua
+++ neovim/.config/nvim/lua/tristan957/init.lua
@@ -2,14 +2,15 @@
 require("tristan957.project-settings").setup({
   fs_root = vim.fs.joinpath(vim.env.HOME, "Projects"),
   rtps_root = vim.fs.joinpath(
-  vim.fn.stdpath("config") --[[@as string]],
-  "lua",
-  "tristan957",
-  "runtimepaths"
+    vim.fn.stdpath("config") --[[@as string]],
+    "lua",
+    "tristan957",
+    "runtimepaths"
   ),
 })
 
 require("tristan957.buffers")
+require("tristan957.chcompdb")
 require("tristan957.diagnostics")
 require("tristan957.linenumbers")
 require("tristan957.lsp")
