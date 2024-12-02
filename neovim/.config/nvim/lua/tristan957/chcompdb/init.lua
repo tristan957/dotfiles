5a5032e2f2f355c769e671a8d7c5acf5d23bbc6e
Tristan Partin <tristan@partin.io>
2024-12-01 23:36:34 -0600

    Change when Chcompdb user command is created
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/tristan957/chcompdb/init.lua
+++ neovim/.config/nvim/lua/tristan957/chcompdb/init.lua
@@ -1,6 +1,8 @@
+local M = {}
+
 ---Run chcompdb to change the compilation database
 ---@param compdb string
-local function chcompdb(compdb)
+M.chcompdb = function(compdb)
   vim.system({ "chcompdb", "--quiet", compdb }, {}, function(o)
     if o.code ~= 0 then
       vim.notify(o.stderr, vim.log.levels.ERROR)
@@ -11,28 +13,4 @@ local function chcompdb(compdb)
   end)
 end
 
-vim.api.nvim_create_user_command("Chcompdb", function(opts)
-  if #opts.fargs == 0 then
-    local fzf_lua = require("fzf-lua")
-
-    fzf_lua.fzf_exec("find -type f -name 'compile_commands.*'", {
-      prompt = "Compilation Database> ",
-      actions = {
-        ["default"] = function(selected)
-          chcompdb(selected[1])
-        end,
-      },
-      -- This isn't working as expected for some reason
-      -- fn_transform = function(db)
-      --   return fzf_lua.make_entry.file(db, { file_icons = true, color_icons = true })
-      -- end,
-    })
-  elseif #opts.fargs == 1 then
-    chcompdb(opts.fargs[1])
-  else
-    vim.notify("Too many arguments passed to chcompdb", vim.log.levels.ERROR)
-  end
-end, {
-  nargs = "?",
-  complete = "file",
-})
+return M
--- neovim/.config/nvim/lua/tristan957/lsp/config/clangd.lua
+++ neovim/.config/nvim/lua/tristan957/lsp/config/clangd.lua
@@ -12,4 +12,36 @@ return {
     "--pch-storage=disk",
     "--header-insertion-decorators",
   },
+  on_init = function()
+    vim.api.nvim_create_user_command("Chcompdb", function(opts)
+      local chcompdb = require("tristan957.chcompdb").chcompdb
+
+      if #opts.fargs == 0 then
+        local fzf_lua = require("fzf-lua")
+
+        fzf_lua.fzf_exec("find -type f -name 'compile_commands.*'", {
+          prompt = "Compilation Database> ",
+          actions = {
+            ["default"] = function(selected)
+              chcompdb(selected[1])
+            end,
+          },
+          -- This isn't working as expected for some reason
+          -- fn_transform = function(db)
+          --   return fzf_lua.make_entry.file(db, { file_icons = true, color_icons = true })
+          -- end,
+        })
+      elseif #opts.fargs == 1 then
+        chcompdb(opts.fargs[1])
+      else
+        vim.notify("Too many arguments passed to chcompdb", vim.log.levels.ERROR)
+      end
+    end, {
+      nargs = "?",
+      complete = "file",
+    })
+  end,
+  on_exit = function()
+    vim.api.nvim_del_user_command("Chcompdb")
+  end,
 }
