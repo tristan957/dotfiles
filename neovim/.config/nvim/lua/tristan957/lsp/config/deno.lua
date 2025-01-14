034d3fb51340e2239bf6ac2017ee7c10ff3f6efe
Tristan Partin <tristan@partin.io>
2025-01-13 23:26:33 -0600

    Disable deno language server unless it is a deno project
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/tristan957/lsp/config/deno.lua
+++ neovim/.config/nvim/lua/tristan957/lsp/config/deno.lua
@@ -2,4 +2,10 @@
 
 ---@type lspconfig.Config
 ---@diagnostic disable-next-line: missing-fields
-return {}
+return {
+  -- Disable deno unless we know for sure that this is a Deno project
+  enabled = (function()
+    local _, err = vim.uv.fs_stat("deno.json")
+    return err == nil
+  end)(),
+}
