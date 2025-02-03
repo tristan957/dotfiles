a382688e6b6f61973ef45e5663aa4cc2192c64a5
Tristan Partin <tristan@partin.io>
2025-02-03 00:47:45 -0600

    Fix pkl treesitter setup

--- neovim/.config/nvim/lua/plugins/pkl/init.lua
+++ neovim/.config/nvim/lua/plugins/pkl/init.lua
@@ -4,12 +4,7 @@
 return {
   "apple/pkl-neovim",
   ft = "pkl",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
   build = function()
     require("pkl-neovim.internal").init()
-
-    vim.cmd("TSInstall! pkl")
   end,
 }
