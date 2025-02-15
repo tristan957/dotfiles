0133d933d5e0caf38263066f9ceb06c54620a14d
Tristan Partin <tristan@partin.io>
2025-02-15 11:54:20 -0600

    Add a dashboard on neovim start
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/snacks/init.lua
+++ neovim/.config/nvim/lua/plugins/snacks/init.lua
@@ -10,7 +10,73 @@ return {
       enabled = false,
     },
     dashboard = {
-      enabled = false,
+      enabled = true,
+      preset = {
+        header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = ":lua Snacks.picker.smart()",
+          },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+          },
+          {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files",
+            action = ":lua Snacks.dashboard.pick('oldfiles')",
+          },
+          {
+            icon = " ",
+            key = "b",
+            desc = "Checkout Branch",
+            action = ":lua Snacks.picker.git_branches()",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          {
+            icon = "󰒲 ",
+            key = "L",
+            desc = "Lazy",
+            action = ":Lazy",
+            enabled = package.loaded.lazy ~= nil,
+          },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup", padding = 1 },
+        {
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+      },
     },
     gitbrowse = {
       url_patterns = {
