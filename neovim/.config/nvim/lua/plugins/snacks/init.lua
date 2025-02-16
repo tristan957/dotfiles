da93e71ab3f82d552b5ec06c9998dc75902397ab
Tristan Partin <tristan@partin.io>
2025-02-15 23:09:08 -0600

    Add Mason section to dashboard
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/snacks/init.lua
+++ neovim/.config/nvim/lua/plugins/snacks/init.lua
@@ -57,6 +57,12 @@ return {
             action = ":Lazy",
             enabled = package.loaded.lazy ~= nil,
           },
+          {
+            icon = " ",
+            key = "M",
+            desc = "Mason",
+            action = ":Mason",
+          },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
