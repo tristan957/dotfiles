68e0a2625fc44e2f1384c4d92de98cd832619284
Tristan Partin <tristan@partin.io>
2025-04-15 17:35:42 -0500

    Fix codecompanion filetype plugin
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/after/ftplugin/codecompanion.lua
+++ neovim/.config/nvim/after/ftplugin/codecompanion.lua
@@ -1,2 +1,5 @@
-vim.opt_local.colorcolumn = {}
-vim.opt_local.showbreak = "NONE"
+-- For some reason, this has to be scheduled
+vim.schedule(function()
+  vim.opt_local.colorcolumn = {}
+  vim.opt_local.showbreak = "NONE"
+end)
