bacadce1458b83852276d433545dc289f6b4133e
Tristan Partin <tristan@partin.io>
2025-04-19 09:57:37 -0500

    Inline ruff into .build.yml

--- scripts/ruff.sh
+++ scripts/ruff.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-files=$(find . -name "*.py" -type f)
-
 # shellcheck disable=SC2086
-ruff check --diff \
-    programs/.local/bin/chcompdb \
-    programs/.local/bin/xdp-file-chooser \
-    $files
+ruff check --diff .
