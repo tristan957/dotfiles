28f9dfa3af08203ca99e6a1502e4b89ba67a364d
Tristan Partin <tristan@partin.io>
2025-05-07 10:26:16 -0500

    Lazy sync

--- neovim/.config/nvim/lua/plugins/mason/init.lua
+++ neovim/.config/nvim/lua/plugins/mason/init.lua
@@ -3,15 +3,12 @@
 
 ---@type LazySpec
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   enabled = true,
   build = ":MasonUpdate",
   event = "VimEnter",
   ---@type MasonSettings
   opts = {
     log_level = vim.log.levels.OFF,
-    ui = {
-      border = vim.o.winborder,
-    },
   },
 }
