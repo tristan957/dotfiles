67e5c00dfcb46a58ec92b4ec2e159a06452742a0
Tristan Partin <tristan@partin.io>
2025-05-13 17:15:00 -0500

    Turn 'list' off when markview is rendering
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/markview/init.lua
+++ neovim/.config/nvim/lua/plugins/markview/init.lua
@@ -17,6 +17,23 @@ return {
         "codecompanion",
         "markdown",
       },
+      callbacks = {
+        on_attach = function(_, wins)
+          vim.iter(wins):each(function(winid)
+            vim.wo[winid].list = false
+          end)
+        end,
+        on_detach = function(_, wins)
+          vim.iter(wins):each(function(winid)
+            vim.wo[winid].list = true
+          end)
+        end,
+        on_mode_change = function(_, wins, mode)
+          vim.iter(wins):each(function(winid)
+            vim.wo[winid].list = mode == "i"
+          end)
+        end,
+      },
       ignore_buftypes = {},
     },
   },
