008962eb063d6824dda6e861e4ceec5bf7a13346
Tristan Partin <tristan@partin.io>
2025-06-18 17:13:00 -0500

    Change location of python history file with env var
    Python 3.13+
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- python/.config/bash.d/python.sh
+++ python/.config/bash.d/python.sh
@@ -1,3 +1,3 @@
-export PYTHONSTARTUP=$HOME/dotfiles/python/startup.py
+export PYTHON_HISTORY="${XDG_STATE_HOME:-$HOME/.var}/python/history"
 # Python virtual environments should stop messing with PS1
 export VIRTUAL_ENV_DISABLE_PROMPT=1
