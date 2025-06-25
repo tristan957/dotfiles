89d386449254e99c87b002a17f1c233457d36590
Tristan Partin <tristan@partin.io>
2025-06-25 15:44:15 -0500

    Fix bash HISTFILE issues
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- bash/.config/bash.d/history.sh
+++ bash/.config/bash.d/history.sh
@@ -1,4 +1,4 @@
 HISTCONTROL='ignoredups:ignorespace'
-HISTFILE="${XDG_STATE_HOME:-$HOME/.var/bash/.bash_history}"
+HISTFILE="${XDG_STATE_HOME:-$HOME/.var}/bash/history"
 HISTSIZE=1000000
 HISTTIMEFORMAT='%FT%T%z: '
