f8496f67f0c4102e818736f555cf685c18aaf45f
Tristan Partin <tristan@partin.io>
2025-06-26 00:45:11 -0500

    Clean up shellcheck script
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-files=$(find . -name "*.sh" -type f \
-    -not -path "./rust/.opt/cargo/*")
+files=$(find . -name "*.sh" -type f)
 
 # shellcheck disable=SC2086
 shellcheck --shell bash --source-path bash -x \
