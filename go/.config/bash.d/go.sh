1a602896584b50821e05faf983a6d146f4852c69
Tristan Partin <tristan@partin.io>
2025-06-30 00:30:31 -0500

    Adding $GOPATH/bin to PATH is unnecessary
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- go/.config/bash.d/go.sh
+++ go/.config/bash.d/go.sh
@@ -1,7 +1,6 @@
+# Will be added to PATH in programs.fish
 export GOBIN="$HOME/.local/bin"
 export GOMODCACHE="${XDG_CACHE_HOME:-$HOME/.cache}/go"
 export GOPATH="${XDG_DATA_HOME:-$HOME/.local/share}/go"
 export GOPROXY=direct
 export GOTELEMETRY=off
-
-export PATH="$GOPATH/bin:$PATH"
--- go/.config/fish/conf.d/go.fish
+++ go/.config/fish/conf.d/go.fish
@@ -1,7 +1,6 @@
+# Will be added to PATH in programs.fish
 set -gx GOBIN "$HOME/.local/bin"
 set -gx GOMODCACHE "$XDG_CACHE_HOME/go"
 set -gx GOPATH "$XDG_DATA_HOME/go"
 set -gx GOPROXY direct
 set -gx GOTELEMETRY off
-
-set -U fish_user_paths "$GOPATH/bin" $fish_user_paths
