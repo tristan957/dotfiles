0dc66271cb222460bd3133b8ee1c28da113eb136
Tristan Partin <tristan@partin.io>
2025-07-01 13:47:37 -0500

    Clean up homebrew path setup in fish

--- homebrew/.config/fish/conf.d/01-homebrew.fish
+++ homebrew/.config/fish/conf.d/01-homebrew.fish
@@ -1,8 +1,10 @@
-if test $IS_MACOS -eq 1
+switch (uname)
+case Darwin
     /opt/homebrew/bin/brew shellenv | source
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/curl/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/man-db/libexec/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/llvm/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/postgresql@17/bin"
 end
+
+fish_add_path --prepend "$HOMEBREW_PREFIX/opt/curl/bin"
+fish_add_path --prepend "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
+fish_add_path --prepend "$HOMEBREW_PREFIX/opt/man-db/libexec/bin"
+fish_add_path --prepend "$HOMEBREW_PREFIX/opt/llvm/bin"
+fish_add_path --prepend "$HOMEBREW_PREFIX/opt/postgresql@17/bin"
