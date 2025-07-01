b095831db8e023908b4c0032b362217968daaea4
Tristan Partin <tristan@partin.io>
2025-07-01 13:48:09 -0500

    Add homebrew and macos sdk paths to MANPATH

--- man/.config/bash.d/man.sh
+++ man/.config/bash.d/man.sh
@@ -1,3 +1,11 @@
 if command -v nvim &>/dev/null; then
     export MANPAGER='nvim +Man!'
 fi
+
+if command -v xcrun &>/dev/null; then
+    export MANPATH="$(xcrun --show-sdk-path)/usr/share/man:$MANPATH"
+fi
+
+if [[ -v HOMEBREW_PREFIX ]]; then
+    export MANPATH="$HOMEBREW_PREFIX/share/man:$MANPATH"
+fi
--- man/.config/fish/conf.d/man.fish
+++ man/.config/fish/conf.d/man.fish
@@ -1,3 +1,11 @@
 if command --query nvim
     set -gx MANPAGER 'nvim +Man!'
 end
+
+if command --query xcrun
+    set -gx --prepend MANPATH "$(xcrun --show-sdk-path)/usr/share/man"
+end
+
+if set --query HOMEBREW_PREFIX
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/share/man"
+end
