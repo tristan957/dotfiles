02aa68fb49644112ae08b42be99231b2d9737df7
Tristan Partin <tristan@partin.io>
2025-07-13 23:25:00 -0500

    Add libtool binaries to PATH
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- homebrew/.config/bash.d/01-homebrew.sh
+++ homebrew/.config/bash.d/01-homebrew.sh
@@ -2,6 +2,7 @@ if [[ $IS_MACOS -eq 1 ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
     export PATH="$HOMEBREW_PREFIX/opt/curl/bin:$PATH"
     export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
+    export PATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin:$PATH"
     export PATH="$HOMEBREW_PREFIX/opt/man-db/libexec/bin:$PATH"
     export PATH="$HOMEBREW_PREFIX/opt/llvm/bin:$PATH"
     export PATH="$HOMEBREW_PREFIX/opt/postgresql@17/bin:$PATH"
