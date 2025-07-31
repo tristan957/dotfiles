4594f25cf316939643a7cf8c3d39b791c390c00a
Tristan Partin <tristan@partin.io>
2025-07-30 23:51:21 -0500

    Add xcode man page paths to MANPATH
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- man/.config/fish/conf.d/man.fish
+++ man/.config/fish/conf.d/man.fish
@@ -4,4 +4,10 @@ end
 
 if set --query HOMEBREW_PREFIX
     set -gx --prepend MANPATH "$HOMEBREW_PREFIX/share/man"
+
+    if command --query xcode-select
+        for path in (xcode-select --show-manpaths)
+            set -gx --prepend MANPATH "$path"
+        end
+    end
 end
