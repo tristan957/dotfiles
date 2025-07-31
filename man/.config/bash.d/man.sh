c6e483648ddfef4b24a9dd3bed55ec53b55df6e9
Tristan Partin <tristan@partin.io>
2025-07-31 00:11:51 -0500

    Append various Homebrew paths to MANPATH
    Anything that conflicts with the system has to be manually added.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- man/.config/bash.d/man.sh
+++ man/.config/bash.d/man.sh
@@ -3,5 +3,19 @@ if command -v nvim &>/dev/null; then
 fi
 
 if [[ -v HOMEBREW_PREFIX ]]; then
+    if command -v xcode-select; then
+        for path in $(xcode-select --show-manpaths); do
+            export MANPATH="$path:$MANPATH"
+        done
+    fi
+
     export MANPATH="$HOMEBREW_PREFIX/share/man:$MANPATH"
+
+    export MANPATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
+    export MANPATH="$HOMEBREW_PREFIX/opt/curl/share/man:$MANPATH"
+    export MANPATH="$HOMEBREW_PREFIX/opt/findutils/share/man:$MANPATH"
+    export MANPATH="$HOMEBREW_PREFIX/opt/grep/libexec/gnuman:$MANPATH"
+    export MANPATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnuman:$MANPATH"
+    export MANPATH="$HOMEBREW_PREFIX/opt/make/libexec/gnuman:$MANPATH"
+    export MANPATH="$HOMEBREW_PREFIX/opt/man-db/libexec/man:$MANPATH"
 fi
--- man/.config/fish/conf.d/man.fish
+++ man/.config/fish/conf.d/man.fish
@@ -3,11 +3,19 @@ if command --query nvim
 end
 
 if set --query HOMEBREW_PREFIX
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/share/man"
-
     if command --query xcode-select
         for path in (xcode-select --show-manpaths)
             set -gx --prepend MANPATH "$path"
         end
     end
+
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/share/man"
+
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman"
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/curl/share/man"
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/findutils/share/man"
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/grep/libexec/gnuman"
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/libtool/libexec/gnuman"
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/make/libexec/gnuman"
+    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/man-db/libexec/man"
 end
