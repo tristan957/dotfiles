f0f0c0b574fccf8c79cdbb4854323dfad376142f
Tristan Partin <tristan@partin.io>
2025-07-31 00:23:17 -0500

    Clean up fish title on Linux
    Ghostty and Ptyxis include the current working directory in the subtitle
    of the window.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- fish/.config/fish/conf.d/title.fish
+++ fish/.config/fish/conf.d/title.fish
@@ -1,9 +1,18 @@
 function fish_title
     set -l cwd (prompt_pwd --dir-length=1)
 
-    if set --query argv[1]
-        echo $cwd: $argv
-    else
-        echo $cwd
+    switch (uname)
+    case Darwin
+        if set --query argv[1]
+            echo $cwd: $argv
+        else
+            echo $cwd
+        end
+    case '*'
+        if set --query argv[1]
+            echo $argv
+        else
+            echo $cwd
+        end
     end
 end
