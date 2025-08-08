04b3c80b7387a90d5136378dcb725351ba581b62
Tristan Partin <tristan@partin.io>
2025-08-08 14:33:18 -0500

    Handle LspProgress event with nil percentage
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/tristan957/lsp/progress.lua
+++ neovim/.config/nvim/lua/tristan957/lsp/progress.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("LspProgress", {
     elseif value.kind == "end" then
       io.stdout:write("\027]9;4;0\027\\")
     elseif value.kind == "report" then
-      io.stdout:write(string.format("\027]9;4;1;%d\027\\", value.percentage))
+      io.stdout:write(string.format("\027]9;4;1;%d\027\\", value.percentage or 0))
     end
   end,
 })
