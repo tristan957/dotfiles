7ab0c612f65c90d74adabda6a137c10be9ee7d7e
Tristan Partin <tristan@partin.io>
2025-08-14 00:40:01 -0500

    Make nvm setup more robust for Homebrew
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- nvm/.config/bash.d/nvm.sh
+++ nvm/.config/bash.d/nvm.sh
@@ -1,3 +1,12 @@
-export NVM_DIR="$HOME/.nvm"
-[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
-[[ -s "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"
+if [[ -v LC_DB_DEVBOX ]]; then
+    export NVM_DIR="$HOME/.nvm"
+else
+    export NVM_DIR="$HOME/.opt/nvm"
+fi
+
+if [[ -v HOMEBREW_PREFIX ]]; then
+    [[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ]] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+else
+    [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
+    [[ -s "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"
+fi
