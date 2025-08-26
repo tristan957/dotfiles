bd36e3a2959d7de5b2c1ca1cb8fef07420f51c0c
Tristan Partin <tristan@partin.io>
2025-08-25 23:29:33 -0500

    Pull apart treesitter.lua into plugins
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ neovim/.config/nvim/plugin/gotmpl.lua
@@ -0,0 +1,7 @@
+vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
+  local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr --[[@as integer]]))
+  local _, _, ext, _ = string.find(fname, ".*%.(%a+)(%.%a+)")
+  if ext ~= nil then
+    metadata["injection.language"] = ext
+  end
+end, {})
