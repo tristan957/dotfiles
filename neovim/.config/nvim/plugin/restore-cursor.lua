5cded6aa7fc553403ef861a263735d678ab27897
Tristan Partin <tristan@partin.io>
2025-08-28 15:11:33 -0500

    Remove mini.misc
    I only used it for cursor restoring, so here is a custom plugin from
    a Neovim issue.
    
    Link: https://github.com/neovim/neovim/issues/16339
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ neovim/.config/nvim/plugin/restore-cursor.lua
@@ -0,0 +1,27 @@
+local ignored_filetypes = {
+  "gitcommit",
+  "gitrebase",
+  "hgcommit",
+  "svn",
+}
+
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function(opts)
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      once = true,
+      buffer = opts.buf,
+      callback = function()
+        local ft = vim.bo[opts.buf].filetype
+        local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+
+        if
+          not vim.tbl_contains(ignored_filetypes, ft)
+          and last_known_line > 1
+          and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+        then
+          vim.api.nvim_feedkeys([[g`"zz]], "nx", false)
+        end
+      end,
+    })
+  end,
+})
