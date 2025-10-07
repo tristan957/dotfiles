466327f78642e99357b3aabd6dbfd88ea2851740
Tristan Partin <tristan@partin.io>
2025-10-07 10:14:00 +0200

    Disable treesitter-textobjects if no tree-sitter CLI is available
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/treesitter-textobjects/init.lua
+++ neovim/.config/nvim/lua/plugins/treesitter-textobjects/init.lua
@@ -3,6 +3,7 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  cond = require("tristan957.treesitter").is_cli_available(),
   enabled = true,
   branch = vim.fn.has("nvim-0.11") == 1 and "main" or "master",
   dependencies = {
