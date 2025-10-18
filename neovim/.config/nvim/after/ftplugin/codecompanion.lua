b21ca55deaf0e840a9be47c0863d41d6c66e02aa
Tristan Partin <tristan@partin.io>
2025-10-18 09:55:32 -0500

    Remove vim.schedule() in codecompanion ftplugin
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/after/ftplugin/codecompanion.lua
+++ neovim/.config/nvim/after/ftplugin/codecompanion.lua
@@ -1,5 +1,2 @@
--- For some reason, this has to be scheduled
-vim.schedule(function()
-  vim.opt_local.colorcolumn = {}
-  vim.opt_local.showbreak = "NONE"
-end)
+vim.opt_local.colorcolumn = {}
+vim.opt_local.showbreak = "NONE"
