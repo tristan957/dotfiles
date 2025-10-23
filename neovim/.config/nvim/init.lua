12bd738b27d00f848a20051d09d38f45de148592
Tristan Partin <tristan@partin.io>
2025-10-23 00:54:40 -0500

    Fix winboder on lazy_backdrop windows
    Link: https://github.com/folke/lazy.nvim/issues/1951#issuecomment-2860253949
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -114,8 +114,18 @@ require("lazy").setup("plugins", {
     enabled = true,
   },
   ui = {
-    border = vim.fn.has("nvim-0.11") == 1 and vim.o.winborder or "single",
+    border = vim.fn.has("+winborder") == 1 and vim.o.winborder or "single",
   },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "User: fix backdrop for lazy window",
+  pattern = "lazy_backdrop",
+  group = vim.api.nvim_create_augroup("lazynvim-fix", { clear = true }),
+  callback = function(ctx)
+    local win = vim.fn.win_findbuf(ctx.buf)[1]
+    vim.api.nvim_win_set_config(win, { border = "none" })
+  end,
 })
 
 vim.cmd.packadd("cfilter")
