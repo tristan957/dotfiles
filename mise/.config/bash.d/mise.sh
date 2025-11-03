9e6c5a784cfc384f70c98ef623836a3049a465b8
Tristan Partin <tristan@partin.io>
2025-11-02 21:31:21 -0600

    Fix MISE_LOG_FILE bash environment variable export
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- mise/.config/bash.d/mise.sh
+++ mise/.config/bash.d/mise.sh
@@ -2,7 +2,7 @@ if [[ $IS_INTERACTIVE -eq 0 ]]; then
     return
 fi
 
-export MISE_LOG_FILE "$XDG_STATE_HOME/mise/mise.log"
+export MISE_LOG_FILE="$XDG_STATE_HOME/mise/mise.log"
 
 if command -v mise &>/dev/null; then
     eval "$(mise activate bash 2>/dev/null)"
