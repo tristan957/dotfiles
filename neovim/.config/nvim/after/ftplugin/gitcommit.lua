c2421f692438447b0c8c0c7236dfa5ece366766c
Tristan Partin <tristan@partin.io>
2025-11-10 12:55:19 -0600

    Use 4 space tabs in gitcommit filetypes
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/after/ftplugin/gitcommit.lua
+++ neovim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -1,5 +1,8 @@
+vim.opt_local.colorcolumn = { "72" }
 vim.opt_local.expandtab = false
 vim.opt_local.formatoptions = "1jntcql/r"
-vim.opt_local.colorcolumn = { "72" }
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
 
 vim.b.minitrailspace_disable = true
--- vim/.config/vim/after/ftplugin/gitcommit.vim
+++ vim/.config/vim/after/ftplugin/gitcommit.vim
@@ -1,3 +1,6 @@
-set colorcolumn=72
-set formatoptions=1jntcql/r
-set noexpandtab
+setlocal colorcolumn=72
+setlocal formatoptions=1jntcql/r
+setlocal noexpandtab
+setlocal shiftwidth=4
+setlocal softtabstop=4
+setlocal tabstop=4
