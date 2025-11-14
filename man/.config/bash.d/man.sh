4dbfceb18ccb1e3f9bae08c0af41e66366f78dd2
Tristan Partin <tristan@partin.io>
2025-11-14 12:04:26 -0600

    Clean up the MANPATH manipulation on macos and homebrew
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- man/.config/bash.d/man.sh
+++ man/.config/bash.d/man.sh
@@ -2,20 +2,22 @@ if command -v nvim &>/dev/null; then
     export MANPAGER='nvim +Man!'
 fi
 
-if [[ -v HOMEBREW_PREFIX ]]; then
-    if command -v xcode-select &>/dev/null; then
+if [[ $IS_MACOS -eq 1 ]]; then
+    if [[ "$(command -v man)" != /usr/bin/man ]] && command -v xcode-select &>/dev/null; then
         for path in $(xcode-select --show-manpaths); do
             export MANPATH="$path:$MANPATH"
         done
     fi
 
-    export MANPATH="$HOMEBREW_PREFIX/share/man:$MANPATH"
+    if [[ -v $HOMEBREW_PREFIX ]]; then
+        export MANPATH="$HOMEBREW_PREFIX/share/man:$MANPATH"
 
-    export MANPATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
-    export MANPATH="$HOMEBREW_PREFIX/opt/curl/share/man:$MANPATH"
-    export MANPATH="$HOMEBREW_PREFIX/opt/findutils/share/man:$MANPATH"
-    export MANPATH="$HOMEBREW_PREFIX/opt/grep/libexec/gnuman:$MANPATH"
-    export MANPATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnuman:$MANPATH"
-    export MANPATH="$HOMEBREW_PREFIX/opt/make/libexec/gnuman:$MANPATH"
-    export MANPATH="$HOMEBREW_PREFIX/opt/man-db/libexec/man:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/curl/share/man:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/findutils/share/man:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/grep/libexec/gnuman:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnuman:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/make/libexec/gnuman:$MANPATH"
+        export MANPATH="$HOMEBREW_PREFIX/opt/man-db/libexec/man:$MANPATH"
+    fi
 fi
--- man/.config/fish/conf.d/man.fish
+++ man/.config/fish/conf.d/man.fish
@@ -2,20 +2,23 @@ if command --query nvim
     set -gx MANPAGER 'nvim +Man!'
 end
 
-if set --query HOMEBREW_PREFIX
-    if command --query xcode-select
+switch (uname)
+case Darwin
+    if test "$(command -v man)" != /usr/bin/man && command --query xcode-select
         for path in (xcode-select --show-manpaths)
             set -gx --prepend MANPATH "$path"
         end
     end
 
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/share/man"
+    if set --query HOMEBREW_PREFIX
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/share/man"
 
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman"
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/curl/share/man"
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/findutils/share/man"
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/grep/libexec/gnuman"
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/libtool/libexec/gnuman"
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/make/libexec/gnuman"
-    set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/man-db/libexec/man"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnuman"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/curl/share/man"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/findutils/share/man"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/grep/libexec/gnuman"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/libtool/libexec/gnuman"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/make/libexec/gnuman"
+        set -gx --prepend MANPATH "$HOMEBREW_PREFIX/opt/man-db/libexec/man"
+    end
 end
