e1c04bee70a698a24fe50eaf5715d972575ac2b1
Tristan Partin <tristan@partin.io>
2025-11-14 10:12:53 -0600

    Only add Homebrew paths on macos
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- homebrew/.config/fish/conf.d/01-homebrew.fish
+++ homebrew/.config/fish/conf.d/01-homebrew.fish
@@ -1,21 +1,21 @@
 switch (uname)
 case Darwin
     /opt/homebrew/bin/brew shellenv | source
+
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/curl/bin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-which/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/pgatch/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/llvm/bin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/make/libexec/gnubin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/man-db/libexec/bin"
+    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/postgresql@18/bin"
+
+    set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/curl/lib/pkgconfig"
 end
-
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/curl/bin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-which/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/pgatch/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/llvm/bin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/make/libexec/gnubin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/man-db/libexec/bin"
-fish_add_path --prepend "$HOMEBREW_PREFIX/opt/postgresql@18/bin"
-
-set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/curl/lib/pkgconfig"
