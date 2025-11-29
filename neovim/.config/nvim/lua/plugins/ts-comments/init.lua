4fb80ca8c5ccbd25a7ece43fe0934878dad6f51b
Tristan Partin <tristan@partin.io>
2025-11-29 10:30:57 -0600

    Set the commentstring for ini files
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/ts-comments/init.lua
+++ neovim/.config/nvim/lua/plugins/ts-comments/init.lua
@@ -1,8 +1,15 @@
 ---@module "lazy"
+---@module "ts-comments"
 
 ---@type LazySpec
 return {
   "folke/ts-comments.nvim",
   enabled = true,
   event = { "BufNewFile", "BufReadPre" },
+  ---@type TSCommentsOptions
+  opts = {
+    lang = {
+      ini = "# %s",
+    },
+  },
 }
