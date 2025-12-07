88b51e9f02dc2fe2f6b058663c12be9e06253502
Tristan Partin <tristan@partin.io>
2025-12-07 14:30:03 -0600

    Cleanup bash-completion sourcing
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- bash/.config/bash.d/completion.sh
+++ bash/.config/bash.d/completion.sh
@@ -1,5 +1,10 @@
-. "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" 2>/dev/null
-. /etc/bash_completion 2>/dev/null
+if [[ -n "$HOMEBREW_PREFIX" ]]; then
+    . "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" 2>/dev/null
+fi
+
+if [[ -f /etc/bash_completion ]]; then
+    . /etc/bash_completion 2>/dev/null
+fi
 
 # shellcheck disable=SC2034
 # GIT_COMPLETION_SHOW_ALL_COMMANDS=1
