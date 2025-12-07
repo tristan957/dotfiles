91db41e55dc666f8007fd71c908ae02636cb59d3
Tristan Partin <tristan@partin.io>
2025-12-07 14:34:47 -0600

    Clean up xdg-terminal-exec setup
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- bootstrap/fedora/xdg-terminal-exec.sh
+++ bootstrap/fedora/xdg-terminal-exec.sh
@@ -1,7 +1,13 @@
 function xdg_terminal_exec_setup() {
-    sudo alternatives --install /usr/local/bin/xdg-terminal-exec \
-        xdg-terminal-exec "$(which ghostty)" 600
-    sudo alternatives --install /usr/local/bin/xdg-terminal-exec \
-        xdg-terminal-exec "$(which ptyxis)" 500
+    if command -v ghostty &>/dev/null; then
+        sudo alternatives --install /usr/local/bin/xdg-terminal-exec \
+            xdg-terminal-exec "$(which ghostty)" 600
+    fi
+
+    if command -v ptyxis &>/dev/null; then
+        sudo alternatives --install /usr/local/bin/xdg-terminal-exec \
+            xdg-terminal-exec "$(which ptyxis)" 500
+    fi
+
     sudo alternatives --auto xdg-terminal-exec
 }
