4bfa4f025d08d9060e5d720a1be1bb03850a14f1
Tristan Partin <tristan@partin.io>
2025-12-07 14:35:01 -0600

    Clean up gh boostrap setup
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- bootstrap/shared/gh.sh
+++ bootstrap/shared/gh.sh
@@ -2,19 +2,10 @@ function gh_login {
     gh auth login --hostname github.com --web --git-protocol https
 }
 
-function gh_bash_completion {
-    systemctl --user enable --now gh-bash-completion.path
-    systemctl --user start gh-bash-completion.service
-}
-
 function gh_setup {
     if ! command -v gh &>/dev/null; then
         builtin return
     fi
 
     gh_login
-
-    if command -v systemctl &>/dev/null; then
-        gh_bash_completion
-    fi
 }
