5d50a6583256104bf9d5e45f950367a16bdc8e6b
Tristan Partin <tristan@partin.io>
2025-12-07 14:35:55 -0600

    Clean up nix bash config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- nix/.config/bash.d/nix.sh
+++ nix/.config/bash.d/nix.sh
@@ -1,1 +1,3 @@
-. "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.sh" 2>/dev/null
+if [[ -f "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.sh" ]]; then
+    . "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.sh" 2>/dev/null
+fi
