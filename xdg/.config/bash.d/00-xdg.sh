f6b0a9c7dbcd51affdefdc66a7ca5014a65ce6ba
Tristan Partin <tristan@partin.io>
2025-12-07 15:49:53 -0600

    Use systemd to define XDG environment variables on !macOS
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- xdg/.config/bash.d/00-xdg.sh
+++ xdg/.config/bash.d/00-xdg.sh
@@ -1,5 +1,7 @@
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_DATA_DIRS="${XDG_DATA_HOME}:$XDG_DATA_DIRS"
-export XDG_STATE_HOME="$HOME/.var"
+if [[ $IS_MACOS -eq 1 ]]; then
+    export XDG_CACHE_HOME="$HOME/.cache"
+    export XDG_CONFIG_HOME="$HOME/.config"
+    export XDG_DATA_HOME="$HOME/.local/share"
+    export XDG_DATA_DIRS="$XDG_DATA_HOME:$XDG_DATA_DIRS"
+    export XDG_STATE_HOME="$HOME/.var"
+fi
--- xdg/.config/fish/conf.d/00-xdg.fish
+++ xdg/.config/fish/conf.d/00-xdg.fish
@@ -1,5 +1,8 @@
-set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx XDG_DATA_HOME "$HOME/.local/share"
-set -gx XDG_DATA_DIRS "$XDG_DATA_HOME:$XDG_DATA_DIRS"
-set -gx XDG_STATE_HOME "$HOME/.var"
+switch "$(uname)"
+case Darwin
+    set -gx XDG_CACHE_HOME "$HOME/.cache"
+    set -gx XDG_CONFIG_HOME "$HOME/.config"
+    set -gx XDG_DATA_HOME "$HOME/.local/share"
+    set -gx XDG_DATA_DIRS "$XDG_DATA_HOME:$XDG_DATA_DIRS"
+    set -gx XDG_STATE_HOME "$HOME/.var"
+end
