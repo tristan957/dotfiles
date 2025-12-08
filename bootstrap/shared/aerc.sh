50f1c5b6cab4c96198a9e54620e112f71e098826
Tristan Partin <tristan@partin.io>
2025-12-08 00:30:05 -0600

    Use a systemd service for fixing the aerc accounts.conf permissions
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- bootstrap/shared/aerc.sh
+++ bootstrap/shared/aerc.sh
@@ -1,7 +1,15 @@
-function aerc_permissions {
+function aerc_enable_unit() {
+    systemctl --user enable --now aerc-accounts-conf.path
+}
+
+function aerc_permissions() {
     chmod 600 "${XDG_CONFIG_HOME:-$HOME/.config}/aerc/accounts.conf"
 }
 
 function aerc_setup {
     aerc_permissions
+
+    if command -v systemctl &>/dev/null; then
+        aerc_enable_unit
+    fi
 }
