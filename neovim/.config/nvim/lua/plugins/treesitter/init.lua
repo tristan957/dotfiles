a9938b0c483651bdf99b2d385fc8b0c82ccd19f8
Tristan Partin <tristan@partin.io>
2025-12-15 17:36:09 -0600

    Clean up nvim-treesitter config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/lua/plugins/treesitter/init.lua
+++ neovim/.config/nvim/lua/plugins/treesitter/init.lua
@@ -13,17 +13,119 @@ return {
   ---@type TSConfig | {}
   opts = {},
   config = function(_, opts)
-    local filetypes = require("tristan957.treesitter").get_filetypes()
-
     require("nvim-treesitter").setup(opts)
-    require("nvim-treesitter").install(filetypes, {
+
+    require("nvim-treesitter").install({
+      "asm",
+      "bash",
+      "blueprint",
+      "c",
+      "c_sharp",
+      "cmake",
+      "codecompanion",
+      "comment",
+      "cpp",
+      "css",
+      "csv",
+      "desktop",
+      "dhall",
+      "diff",
+      -- This grammar sucks ass
+      -- "dockerfile",
+      "doxygen",
+      "editorconfig",
+      "fish",
+      "ghostty",
+      "gitconfig",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "gleam",
+      "glsl",
+      "go",
+      "gomod",
+      "gosum",
+      "gotmpl",
+      "gowork",
+      "gpg",
+      "graphql",
+      "hare",
+      "hcl",
+      "helm",
+      "html",
+      "http",
+      "ini",
+      "java",
+      "javascript",
+      "jq",
+      "json",
+      "json.openapi",
+      "json5",
+      "jsonc",
+      "jsonnet",
+      "just",
+      "kconfig",
+      "kcl",
+      "kdl",
+      "lua",
+      "make",
+      "markdown",
+      "meson",
+      "nginx",
+      "nix",
+      "objdump",
+      "passwd",
+      "perl",
+      "pkl",
+      "po",
+      "powershell",
+      "prisma",
+      "psv",
+      "properties",
+      "pymanifest",
+      "python",
+      "query",
+      "readline",
+      "requirements",
+      "robots",
+      "rust",
+      -- "scfg",
+      "scss",
+      "sh",
+      "sql",
+      "sshconfig",
+      "starlark",
+      "strace",
+      "superhtml",
+      "swift",
+      "tiltfile",
+      "tmux",
+      "toml",
+      "tsv",
+      "typescript",
+      "typst",
+      "vhs",
+      "vim",
+      "xml",
+      "yaml",
+      "yaml.openapi",
+      "zig",
+      "ziggy",
+      "ziggy_schema",
+    }, {
       summary = false,
     } --[[@as InstallOptions]])
 
     vim.api.nvim_create_autocmd("FileType", {
       group = require("tristan957.treesitter").augroup,
-      pattern = filetypes,
       callback = function(ev)
+        local filetype = ev.match
+
+        if not require("tristan957.treesitter").has_parser(filetype) then
+          return
+        end
+
         vim.bo[ev.buf].indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
         vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
         vim.wo[0][0].foldmethod = "expr"
--- neovim/.config/nvim/lua/tristan957/treesitter/init.lua
+++ neovim/.config/nvim/lua/tristan957/treesitter/init.lua
@@ -6,121 +6,21 @@ M.augroup = vim.api.nvim_create_augroup("tristan957_treesitter", { clear = true 
 ---@param ft string
 ---@return boolean
 M.has_parser = function(ft)
-  return pcall(vim.treesitter.language.add, vim.treesitter.language.get_lang(ft) or ft)
+  local language = vim.treesitter.language.get_lang(ft) or ft
+  local loaded, err = vim.treesitter.language.add(language)
+  if err ~= nil then
+    return false
+  end
+
+  assert(loaded ~= nil)
+
+  return loaded
 end
 
 --- Check whether the tree-sitter CLI is available
 ---@return boolean
 M.is_cli_available = function()
   return vim.fn.executable("tree-sitter") == 1
-end
-
---- Get filetypes which have treesitter support according to my configuration.
----@return string[]
-M.get_filetypes = function()
-  if not M.is_cli_available() then
-    return {}
-  end
-
-  return {
-    "asm",
-    "bash",
-    "blueprint",
-    "c",
-    "c_sharp",
-    "cmake",
-    "codecompanion",
-    "comment",
-    "cpp",
-    "css",
-    "csv",
-    "desktop",
-    "dhall",
-    "diff",
-    -- This grammar sucks ass
-    -- "dockerfile",
-    "doxygen",
-    "editorconfig",
-    "fish",
-    "ghostty",
-    "gitconfig",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "gleam",
-    "glsl",
-    "go",
-    "gomod",
-    "gosum",
-    "gotmpl",
-    "gowork",
-    "gpg",
-    "graphql",
-    "hare",
-    "hcl",
-    "helm",
-    "html",
-    "http",
-    "ini",
-    "java",
-    "javascript",
-    "jq",
-    "json",
-    "json.openapi",
-    "json5",
-    "jsonc",
-    "jsonnet",
-    "just",
-    "kconfig",
-    "kcl",
-    "kdl",
-    "lua",
-    "make",
-    "markdown",
-    "meson",
-    "nginx",
-    "nix",
-    "objdump",
-    "passwd",
-    "perl",
-    "pkl",
-    "po",
-    "powershell",
-    "prisma",
-    "psv",
-    "properties",
-    "pymanifest",
-    "python",
-    "query",
-    "readline",
-    "requirements",
-    "robots",
-    "rust",
-    -- "scfg",
-    "scss",
-    "sh",
-    "sql",
-    "sshconfig",
-    "starlark",
-    "strace",
-    "superhtml",
-    "swift",
-    "tiltfile",
-    "tmux",
-    "toml",
-    "tsv",
-    "typescript",
-    "typst",
-    "vhs",
-    "vim",
-    "xml",
-    "yaml",
-    "yaml.openapi",
-    "zig",
-    "ziggy",
-    "ziggy_schema",
-  }
 end
 
 return M
