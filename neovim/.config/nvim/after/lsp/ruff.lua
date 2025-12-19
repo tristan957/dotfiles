8604da55edaf5ef2df8bb1cbc483f0dfdf14c6c2
Tristan Partin <tristan@partin.io>
2025-12-19 00:57:15 -0600

    Clean up ruff neovim config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/after/lsp/ruff.lua
+++ neovim/.config/nvim/after/lsp/ruff.lua
@@ -1,21 +1,23 @@
 ---@type vim.lsp.Config
 return {
+  cmd = { "ruff", "server" },
   before_init = function(_, config)
     local poetry = require("tristan957.utils.poetry")
-    local default_config = require("lspconfig.configs.ruff").default_config
 
     -- We are already in the virtual environment, so skip the setup.
     if vim.env.VIRTUAL_ENV ~= nil then
       return
     end
 
-    if poetry.is_workspace(vim.uv.cwd() --[[@as string]]) and vim.fn.executable("poetry") == 1 then
+    if
+      poetry.is_workspace(vim.uv.cwd() --[[@as string]]) and vim.fn.executable("poetry") == 1
+    then
       -- Check if ruff is installed in the workspace
       if poetry.run({ "ruff", "--version" }).code ~= 0 then
         return
       end
 
-      config.cmd = { "poetry", "run", "--", "ruff", unpack(default_config.cmd, 2) }
+      config.cmd = { "poetry", "run", "--", "ruff", "server" }
     end
   end,
 }
