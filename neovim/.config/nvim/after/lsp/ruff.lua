e7d4e9c00c86fc2135dc3a33af7afe8edb83bffe
Tristan Partin <tristan@partin.io>
2025-12-25 13:16:22 -0600

    Clean up ruff and ty lsp config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/after/lsp/ruff.lua
+++ neovim/.config/nvim/after/lsp/ruff.lua
@@ -1,17 +1,19 @@
 ---@type vim.lsp.Config
 return {
-  before_init = function(_, config)
-    --We are already in the virtual environment, so skip the setup.
-    if vim.env.VIRTUAL_ENV ~= nil then
-      return
+  cmd = function(dispatchers, _)
+    local cmd = { "ruff", "server" }
+
+    --We are already in the virtual environment, so skip the setup
+    if vim.env.VIRTUAL_ENV == nil then
+      local poetry = require("tristan957.utils.poetry")
+
+      if
+        poetry.is_workspace(vim.uv.cwd() --[[@as string]]) and vim.fn.executable("poetry") == 1
+      then
+        cmd = poetry.run_command_line({ "ruff", "server" })
+      end
     end
 
-    local poetry = require("tristan957.utils.poetry")
-
-    if
-      poetry.is_workspace(vim.uv.cwd() --[[@as string]]) and vim.fn.executable("poetry") == 1
-    then
-      config.cmd = poetry.run_command_line({ "ruff", "server" })
-    end
+    return vim.lsp.rpc.start(cmd, dispatchers)
   end,
 }
--- neovim/.config/nvim/after/lsp/ty.lua
+++ neovim/.config/nvim/after/lsp/ty.lua
@@ -2,18 +2,20 @@
 
 ---@type vim.lsp.Config
 return {
-  before_init = function(_, config)
-    --We are already in the virtual environment, so skip the setup.
-    if vim.env.VIRTUAL_ENV ~= nil then
-      return
+  cmd = function(dispatchers, _)
+    local cmd = { "ty", "server" }
+
+    --We are already in the virtual environment, so skip the setup
+    if vim.env.VIRTUAL_ENV == nil then
+      local poetry = require("tristan957.utils.poetry")
+
+      if
+        poetry.is_workspace(vim.uv.cwd() --[[@as string]]) and vim.fn.executable("poetry") == 1
+      then
+        cmd = poetry.run_command_line({ "ty", "server" })
+      end
     end
 
-    local poetry = require("tristan957.utils.poetry")
-
-    if
-      poetry.is_workspace(vim.uv.cwd() --[[@as string]]) and vim.fn.executable("poetry") == 1
-    then
-      config.cmd = poetry.run_command_line({ "ty", "server" })
-    end
+    return vim.lsp.rpc.start(cmd, dispatchers)
   end,
 }
