b137d1862fd71b3c7e9fd1cd94b6bea11fce92d6
Tristan Partin <tristan@partin.io>
2026-01-03 01:34:51 -0600

    Add injections for build.sr.ht manifests
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/queries/yaml/injections.scm
+++ neovim/.config/nvim/queries/yaml/injections.scm
@@ -9,3 +9,20 @@
 ] @injection.content
   (#lua-match? @injection.content "[$]{{.*}}")
   (#set! injection.language "ghactions"))
+
+; Sourcehut build manifests
+((block_mapping_pair
+    key: (flow_node
+      (plain_scalar (string_scalar) @key))
+    value: (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_mapping
+              (block_mapping_pair
+                value: (block_node
+                  (block_scalar) @injection.content)))))))
+  (#eq? @key "tasks")
+  (#filename-lua-match? ".build.ya?ml")
+  (#set! injection.language "sh")
+))
