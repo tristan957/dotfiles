4f246a468216e57d2c5e628d92e2678055628b02
Tristan Partin <tristan@partin.io>
2026-01-23 16:12:27 -0600

    Remove GOPROXY since it just causes problem
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- go/.config/fish/conf.d/go.fish
+++ go/.config/fish/conf.d/go.fish
@@ -2,5 +2,5 @@
 set -gx GOBIN "$HOME/.local/bin"
 set -gx GOMODCACHE "$XDG_CACHE_HOME/go"
 set -gx GOPATH "$XDG_DATA_HOME/go"
-set -gx GOPROXY direct
+# set -gx GOPROXY direct
 set -gx GOTELEMETRY off
