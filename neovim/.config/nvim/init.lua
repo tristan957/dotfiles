7117dee14fe7fb8539dfc82a853c8ac683db5840
Tristan Partin <tristan@partin.io>
2026-03-08 00:19:20 -0600

    Remove lazyredraw setting
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -40,7 +40,6 @@ vim.opt.incsearch = true
 vim.opt.joinspaces = false
 vim.opt.jumpoptions = { "view" }
 vim.opt.laststatus = 3
-vim.opt.lazyredraw = true
 vim.opt.linebreak = true
 vim.opt.list = true
 vim.opt.listchars = { space = "·", tab = "⭢ " }
