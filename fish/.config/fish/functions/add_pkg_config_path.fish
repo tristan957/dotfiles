de40ec892a9c041492c0c3d74b5b45ff02071754
Tristan Partin <tristan@partin.io>
2026-03-18 16:03:51 -0500

    Add fish function for adding to PKG_CONFIG_PATH
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ fish/.config/fish/functions/add_pkg_config_path.fish
@@ -0,0 +1,19 @@
+function add_pkg_config_path --description "Add paths to PKG_CONFIG_PATH, skipping duplicates"
+    argparse p/prepend a/append -- $argv
+    or return 1
+
+    set -l mode append
+    if set -q _flag_prepend
+        set mode prepend
+    end
+
+    for path in $argv
+        contains -- $path $PKG_CONFIG_PATH; and continue
+        switch $mode
+            case prepend
+                set -gx PKG_CONFIG_PATH $path $PKG_CONFIG_PATH
+            case append
+                set -gxa PKG_CONFIG_PATH $path
+        end
+    end
+end
