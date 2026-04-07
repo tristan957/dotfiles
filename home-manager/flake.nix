a92e4062aaa6428d2a0152148392cb000c503da3
Tristan Partin <tristan@partin.io>
2026-04-07 16:27:13 -0500

    Improve home-manager config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,8 +1,7 @@
 {
-  description = "Home Manager configuration of dbltap";
+  description = "Home Manager configuration";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,19 +12,42 @@
   outputs =
     { nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      mkHome =
+        {
+          system,
+          username,
+          homeDirectory,
+          extraPackages ? (_: [ ]),
+        }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = {
+            inherit username homeDirectory;
+            machinePackages = extraPackages pkgs;
+          };
+          modules = [ ./home.nix ];
+        };
     in
     {
-      homeConfigurations."dbltap" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      homeConfigurations = {
+        "dbltap@macbook" = mkHome {
+          system = "aarch64-darwin";
+          username = "dbltap";
+          homeDirectory = "/Users/dbltap";
+        };
+        "dbltap@dbltap-dev" = mkHome {
+          system = "x86_64-linux";
+          username = "dbltap";
+          homeDirectory = "/home/dbltap";
+        };
+        "tristan957@c-3po" = mkHome {
+          system = "x86_64-linux";
+          username = "tristan957";
+          homeDirectory = "/home/tristan957";
+        };
       };
     };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  username,
+  homeDirectory,
+  machinePackages ? [],
+  ...
+}:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "dbltap";
-  home.homeDirectory = "/home/dbltap";
-  xdg.stateHome = "/home/dbltap/.var";
+  home.username = username;
+  home.homeDirectory = homeDirectory;
+  xdg.stateHome = "${homeDirectory}/.var";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -35,7 +40,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-  ];
+  ] ++ machinePackages;
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
