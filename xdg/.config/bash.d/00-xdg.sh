ae062fd5f3daf83800f0f45dae3c39748935eebb
Tristan Partin <tristan@partin.io>
2026-04-07 17:46:42 -0500

    Set XDG environment variables always
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- xdg/.config/bash.d/00-xdg.sh
+++ xdg/.config/bash.d/00-xdg.sh
@@ -1,7 +1,5 @@
-if [[ $IS_MACOS -eq 1 ]]; then
-    export XDG_CACHE_HOME="$HOME/.cache"
-    export XDG_CONFIG_HOME="$HOME/.config"
-    export XDG_DATA_HOME="$HOME/.local/share"
-    export XDG_DATA_DIRS="$XDG_DATA_HOME:$XDG_DATA_DIRS"
-    export XDG_STATE_HOME="$HOME/.var"
-fi
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_DATA_DIRS="$XDG_DATA_HOME:$XDG_DATA_DIRS"
+export XDG_STATE_HOME="$HOME/.var"
--- xdg/.config/fish/conf.d/00-xdg.fish
+++ xdg/.config/fish/conf.d/00-xdg.fish
@@ -1,8 +1,5 @@
-switch "$(uname)"
-case Darwin
-    set -gx XDG_CACHE_HOME "$HOME/.cache"
-    set -gx XDG_CONFIG_HOME "$HOME/.config"
-    set -gx XDG_DATA_HOME "$HOME/.local/share"
-    set -gx XDG_DATA_DIRS "$XDG_DATA_HOME:$XDG_DATA_DIRS"
-    set -gx XDG_STATE_HOME "$HOME/.var"
-end
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_DATA_DIRS "$XDG_DATA_HOME:$XDG_DATA_DIRS"
+set -gx XDG_STATE_HOME "$HOME/.var"
