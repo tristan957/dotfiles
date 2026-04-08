2c065aecf966831862530910e59793bdbd8fd619
Tristan Partin <tristan@partin.io>
2026-04-08 15:43:41 -0500

    Assume fzf always supports fish
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- fzf/.config/fish/conf.d/90-fzf.fish
+++ fzf/.config/fish/conf.d/90-fzf.fish
@@ -3,9 +3,5 @@ if not status is-interactive
 end
 
 if command --query fzf
-    if fzf --fish &>/dev/null
-        fzf --fish | source
-    else if test -f /usr/share/doc/fzf/examples/key-bindings.fish
-        source /usr/share/doc/fzf/examples/key-bindings.fish
-    end
+    fzf --fish | source
 end
