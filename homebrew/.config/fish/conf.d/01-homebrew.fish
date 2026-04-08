a962959fd695f1929fd4a1f100dd17a065bdc3d1
Tristan Partin <tristan@partin.io>
2026-04-08 15:43:55 -0500

    Optimize adding paths from homebrew to environment variables
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- homebrew/.config/fish/conf.d/01-homebrew.fish
+++ homebrew/.config/fish/conf.d/01-homebrew.fish
@@ -2,25 +2,26 @@ switch (__fish_uname)
 case Darwin
     /opt/homebrew/bin/brew shellenv | source
 
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/bison/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/curl/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/flex/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/gnu-which/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/icu4c/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/icu4c/sbin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/pgatch/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/llvm/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/make/libexec/gnubin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/man-db/libexec/bin"
-    fish_add_path --prepend "$HOMEBREW_PREFIX/opt/postgresql@18/bin"
+    fish_add_path --prepend \
+        "$HOMEBREW_PREFIX/opt/bison/bin" \
+        "$HOMEBREW_PREFIX/opt/curl/bin" \
+        "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/findutils/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/flex/bin" \
+        "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/gnu-which/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/icu4c/bin" \
+        "$HOMEBREW_PREFIX/opt/icu4c/sbin" \
+        "$HOMEBREW_PREFIX/opt/patch/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/llvm/bin" \
+        "$HOMEBREW_PREFIX/opt/make/libexec/gnubin" \
+        "$HOMEBREW_PREFIX/opt/man-db/libexec/bin" \
+        "$HOMEBREW_PREFIX/opt/postgresql@18/bin"
 
-    add_pkg_config_path "$HOMEBREW_PREFIX/opt/curl/lib/pkgconfig"
-    add_pkg_config_path "$HOMEBREW_PREFIX/opt/icu4c/lib/pkgconfig"
+    add_pkg_config_path \
+        "$HOMEBREW_PREFIX/opt/curl/lib/pkgconfig" \
+        "$HOMEBREW_PREFIX/opt/icu4c/lib/pkgconfig"
 end
