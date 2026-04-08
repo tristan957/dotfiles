8bc88b1ddc51d86427d05f43b57a4edc044719ca
Tristan Partin <tristan@partin.io>
2026-04-08 15:07:06 -0500

    Fix config file ordering in bash and fish
    Signed-off-by: Tristan Partin <tristan@partin.io>

---
+++ xdg/.config/fish/conf.d/00-xdg.fish
@@ -0,0 +1,7 @@
+if ! set --query SYSTEMD_ENVIRONMENT_LOADED
+    set -gx XDG_CACHE_HOME "$HOME/.cache"
+    set -gx XDG_CONFIG_HOME "$HOME/.config"
+    set -gx XDG_DATA_HOME "$HOME/.local/share"
+    set -gx --path XDG_DATA_DIRS $XDG_DATA_HOME $XDG_DATA_DIRS
+    set -gx XDG_STATE_HOME "$HOME/.var"
+end
