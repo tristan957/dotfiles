3c37513f06eaa916429d914e18a62d15e0167c3d
Tristan Partin <tristan@partin.io>
2026-04-07 23:57:31 -0500

    Add more tools to dbltap-dev
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- home-manager/.config/home-manager/flake.nix
+++ home-manager/.config/home-manager/flake.nix
@@ -43,13 +43,40 @@
           username = "dbltap";
           homeDirectory = "/home/dbltap";
           extraPackages = pkgs: [
+            pkgs._1password-cli
+            pkgs.asciinema_3
+            pkgs.bat
+            pkgs.bear
+            pkgs.chawan
+            pkgs.delta
+            pkgs.difftastic
+            pkgs.direnv
+            pkgs.fd
             pkgs.fish
             pkgs.fzf
+            pkgs.gh
+            pkgs.glab
+            pkgs.glow
+            pkgs.gum
+            pkgs.hut
+            pkgs.just
+            pkgs.miller
+            pkgs.mold
+            pkgs.muon
             pkgs.neovim
+            pkgs.nushell
+            pkgs.reuse
             pkgs.ripgrep
+            pkgs.rr
+            pkgs.sequin
+            pkgs.sequoia-sq
+            pkgs.shellcheck
+            pkgs.shfmt
             pkgs.stow
+            pkgs.tokei
             pkgs.tmux
             pkgs.tree-sitter
+            pkgs.worktrunk
             pkgs.zellij
             pkgs.zoxide
           ];
