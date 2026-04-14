ec43c77f9fe74080502af2e6ae12f17596087494
Tristan Partin <tristan@partin.io>
2026-04-13 22:05:29 -0700

    Only source nix files if nix was not previously sourced
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- nix/.config/bash.d/70-nix.sh
+++ nix/.config/bash.d/70-nix.sh
@@ -1,3 +1,4 @@
-if [[ -f "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.sh" ]]; then
+if [[ -z "$__NIX_SOURCED" ]] && [[ -f "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.sh" ]]; then
+    export __NIX_SOURCED=1
     . "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.sh" 2>/dev/null
 fi
--- nix/.config/fish/conf.d/70-nix.fish
+++ nix/.config/fish/conf.d/70-nix.fish
@@ -1,1 +1,4 @@
-source "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.fish" 2>/dev/null
+if not set -q __NIX_SOURCED
+    set -gx __NIX_SOURCED 1
+    source "$XDG_STATE_HOME/nix/profile/etc/profile.d/nix.fish" 2>/dev/null
+end
