8e2abcba26d12d841e29b907ec0ee44b67440d84
Tristan Partin <tristan@partin.io>
2026-04-13 21:50:06 -0700

    Only prepend to XDG_DATA_DIRS if XDG_DATA_HOME is not a part of it
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- xdg/.config/fish/conf.d/00-xdg.fish
+++ xdg/.config/fish/conf.d/00-xdg.fish
@@ -2,6 +2,6 @@ if ! set --query SYSTEMD_ENVIRONMENT_LOADED
     set -gx XDG_CACHE_HOME "$HOME/.cache"
     set -gx XDG_CONFIG_HOME "$HOME/.config"
     set -gx XDG_DATA_HOME "$HOME/.local/share"
-    set -gx --path XDG_DATA_DIRS $XDG_DATA_HOME $XDG_DATA_DIRS
+    contains -- $XDG_DATA_HOME $XDG_DATA_DIRS; or set -gx --path XDG_DATA_DIRS $XDG_DATA_HOME $XDG_DATA_DIRS
     set -gx XDG_STATE_HOME "$HOME/.var"
 end
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,7 +2,7 @@ if [ -z "$SYSTEMD_ENVIRONMENT_LOADED" ]; then
     export XDG_CACHE_HOME="$HOME/.cache"
     export XDG_CONFIG_HOME="$HOME/.config"
     export XDG_DATA_HOME="$HOME/.local/share"
-    export XDG_DATA_DIRS="$XDG_DATA_HOME:$XDG_DATA_DIRS"
+    [[ ":$XDG_DATA_DIRS:" != *":$XDG_DATA_HOME:"* ]] && export XDG_DATA_DIRS="$XDG_DATA_HOME:$XDG_DATA_DIRS"
     export XDG_STATE_HOME="$HOME/.var"
 fi
 
