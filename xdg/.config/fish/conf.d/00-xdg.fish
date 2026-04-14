47c3125974cd4ee77f93ab62737d126a06fde448
Tristan Partin <tristan@partin.io>
2026-04-14 05:15:36 +0000

    Fix adding two XDG_DATA_HOME values to XDG_DATA_DIRS
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- xdg/.config/fish/conf.d/00-xdg.fish
+++ xdg/.config/fish/conf.d/00-xdg.fish
@@ -2,6 +2,7 @@ if ! set --query SYSTEMD_ENVIRONMENT_LOADED
     set -gx XDG_CACHE_HOME "$HOME/.cache"
     set -gx XDG_CONFIG_HOME "$HOME/.config"
     set -gx XDG_DATA_HOME "$HOME/.local/share"
-    contains -- $XDG_DATA_HOME $XDG_DATA_DIRS; or set -gx --path XDG_DATA_DIRS $XDG_DATA_HOME $XDG_DATA_DIRS
+    set -gx --path XDG_DATA_DIRS $XDG_DATA_DIRS
+    contains -- $XDG_DATA_HOME $XDG_DATA_DIRS; or set -pgx XDG_DATA_DIRS $XDG_DATA_HOME
     set -gx XDG_STATE_HOME "$HOME/.var"
 end
