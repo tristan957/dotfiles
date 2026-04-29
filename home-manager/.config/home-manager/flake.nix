c8c433ef129e7302c89e499d206f27b12665ff29
Tristan Partin <tristan@partin.io>
2026-04-29 09:31:55 -0500

    Disable direnv's checkPhase
    It doesn't seem to work when I have direnv loaded in my shell.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- home-manager/.config/home-manager/flake.nix
+++ home-manager/.config/home-manager/flake.nix
@@ -26,6 +26,13 @@
 
         # Allow unfree packages like 1Password CLI
         config.allowUnfree = true;
+
+        overlays = [
+          (final: prev: {
+            # direnv's checkPhase seems to struggle if direnv is loaded
+            direnv = prev.direnv.overrideAttrs { doCheck = false; };
+          })
+        ];
       };
     in
       home-manager.lib.homeManagerConfiguration {
