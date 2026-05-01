d157fc38eb38d656075e540898daf5c15c174d63
Tristan Partin <tristan@partin.io>
2026-05-01 18:03:07 -0500

    Attempt to bring back fish in Cloud Desktops
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/cloud-desktop/default.nix
+++ modules/cloud-desktop/default.nix
@@ -1,7 +1,14 @@
-{...}: {
+{lib, ...}: {
   home.sessionVariables = {
     AWS_EC2_METADATA_DISABLED = "true";
     BRAZIL_WORKSPACE_DEFAULT_LAYOUT = "short";
     CLOUD_DESKTOP = "1";
   };
+
+  # Exec into fish as soon as possible in Cloud Desktops
+  programs.zsh.initContent = lib.mkBefore ''
+    if command -v fish &>/dev/null; then
+      exec env SHELL="$(which fish)" fish --login
+    fi
+  '';
 }
