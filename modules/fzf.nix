397e278777978a34547f05117d93002a95c0fce9
Tristan Partin <tristan@partin.io>
2026-05-01 15:02:05 -0500

    Clean up fzf nix config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -1,14 +1,9 @@
-{config, ...}: {
+{...}: {
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
+    defaultOptions = ["--color=16" "--highlight-line"];
   };
-
-  home.sessionVariables = {
-    FZF_DEFAULT_OPTS_FILE = "${config.xdg.configHome}/fzf/fzfrc";
-  };
-
-  xdg.configFile."fzf/fzfrc".source = ../fzf/.config/fzf/fzfrc;
 }
