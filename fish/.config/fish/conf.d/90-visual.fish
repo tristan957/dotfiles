b37119c77a68e5cf7920ef24f6017a6316255374
Tristan Partin <tristan@partin.io>
2026-05-01 01:24:32 -0500

    Remove VISUAL setting
    It is supposed to be different from EDITOR, but I forget how.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- fish/.config/fish/conf.d/90-visual.fish
+++
@@ -1,9 +0,0 @@
-if command --query nvim
-    set -gx VISUAL nvim
-else if command --query vim
-    set -gx VISUAL vim
-else if command --query vi
-    set -gx VISUAL vi
-else
-    set -gx VISUAL nano
-end
