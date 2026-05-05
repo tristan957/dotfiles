fe90dc06a51beee6f864330a1941cb08f8e3c4b7
Tristan Partin <tristan@partin.io>
2026-05-05 06:01:42 +0000

    Add support for a $XDG_CONFIG_HOME/op/service-account-token file
    If it exists, we export OP_SERVICE_ACCOUNT_TOKEN into the shell
    environment.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/1password/default.nix
+++ modules/1password/default.nix
@@ -6,6 +6,24 @@
   options.modules."1password".enable = lib.mkEnableOption "1password";
 
   config = lib.mkIf config.modules."1password".enable {
+    programs.zsh.envExtra = ''
+      if [ -f "$XDG_CONFIG_HOME/op/service-account-token" ]; then
+        export OP_SERVICE_ACCOUNT_TOKEN="$(cat "$XDG_CONFIG_HOME/op/service-account-token")"
+      fi
+    '';
+
+    programs.bash.profileExtra = lib.mkAfter ''
+      if [ -f "$XDG_CONFIG_HOME/op/service-account-token" ]; then
+        export OP_SERVICE_ACCOUNT_TOKEN="$(cat "$XDG_CONFIG_HOME/op/service-account-token")"
+      fi
+    '';
+
+    programs.fish.shellInit = ''
+      if test -f "$XDG_CONFIG_HOME/op/service-account-token"
+        set -gx OP_SERVICE_ACCOUNT_TOKEN (cat "$XDG_CONFIG_HOME/op/service-account-token")
+      end
+    '';
+
     systemd.user.services."1password" = {
       Unit = {
         Description = "Start 1Password";
