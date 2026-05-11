316c1ae2810513ed17e1e771d34b2a19fb22923d
Tristan Partin <tristan@partin.io>
2026-05-11 22:34:00 +0000

    Fix file paths in shellcheck.sh
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -4,9 +4,7 @@ files=$(find . -name "*.sh" -type f)
 
 # shellcheck disable=SC2086
 shellcheck --shell bash --source-path bash -x \
-    aerc/.local/bin/aerc-signature \
-    bash/.bashrc \
-    bash/.bash_logout \
-    bash/.bash_profile \
-    programs/.local/bin/dbgwait \
+    modules/aerc/aerc-signature \
+    modules/bash/bash_logout \
+    modules/programs/dbgwait \
     $files
