72cacc953a8eedf01e0d467904d484fe30f9eedc
Tristan Partin <tristan@partin.io>
2026-05-11 22:35:57 +0000

    Fix formatting in scripts/update-flakes.sh
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- scripts/update-flakes.sh
+++ scripts/update-flakes.sh
@@ -4,6 +4,6 @@ echo 'Updating root flake'
 nix flake update --flake .
 
 for flake in ./flakes/*/; do
-	echo "Updating $(basename "$flake") flake"
-	nix flake update --flake "$flake"
+    echo "Updating $(basename "$flake") flake"
+    nix flake update --flake "$flake"
 done
