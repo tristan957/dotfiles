b2831d243a456e99688b3636079c6e43949cc34f
Tristan Partin <tristan@partin.io>
2026-05-27 22:22:53 -0500

    Disable the nixpkgs release check in home-manager
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- home.nix
+++ home.nix
@@ -14,6 +14,7 @@
     home.username = username;
     home.homeDirectory = homeDirectory;
     home.stateVersion = "25.11";
+    home.enableNixpkgsReleaseCheck = false;
 
     xdg = {
       enable = true;
