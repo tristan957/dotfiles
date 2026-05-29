6b53ef37c327b2a159e552e23ec223f1c5fe186e
Tristan Partin <tristan@partin.io>
2026-05-29 10:07:20 -0500

    Update postgtres flake
    It should just be a simple dev-shell flake.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- flakes/postgres/flake.nix
+++ flakes/postgres/flake.nix
@@ -3,23 +3,98 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs}: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = f:
       nixpkgs.lib.genAttrs systems (system:
         f {
           pkgs = import nixpkgs {inherit system;};
         });
-
-    configModule = import ./lib/config-module.nix;
   in {
-    lib.mkPostgresDeps = import ./lib/mk-postgres-deps.nix {inherit configModule;};
-    checks = import ./checks.nix {inherit self forAllSystems;};
     devShells = forAllSystems ({pkgs}: {
-      default = import ./dev-shells/default.nix {inherit self pkgs;};
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            # Build dependencies
+            autoconf269
+            bison
+            clang
+            flex
+            gcc
+            meson
+            muon
+            ninja
+            perl
+            pkgconf
+            python3
+            samurai
+
+            # Libraries
+            aws-lc
+            curl
+            gettext
+            icu
+            krb5
+            libedit
+            libressl
+            libxml2
+            libxslt
+            llvm
+            lz4
+            openldap
+            openssl
+            readline
+            tcl
+            libuuid
+            zlib
+            zstd
+
+            # Docs
+            ditaa
+            docbook2x
+            docbook_sgml_dtd_41
+            docbook_xsl
+            fop
+            graphviz
+            pandoc
+
+            # Testing
+            perlPackages.IPCRun
+
+            # Dev tools
+            bash-language-server
+            bear
+            binutils
+            ccache
+            clang-tools
+            cppcheck
+            elfutils
+            git
+            include-what-you-use
+            lcov
+            lldb
+            pgformatter
+            postgres-language-server
+            shellcheck
+          ]
+          ++ lib.optionals stdenv.isLinux [
+            avahi-compat
+            gdb
+            libselinux
+            libsepol
+            liburing
+            linux-pam
+            linuxPackages.systemtap
+            ltrace
+            numactl
+            pcre2
+            perf
+            rr
+            strace
+            systemdLibs
+            valgrind
+          ];
+      };
     });
   };
 }
