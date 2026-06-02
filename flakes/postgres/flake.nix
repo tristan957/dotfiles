47ece86c91e59230ec30712b1967f16854982131
Tristan Partin <tristan@partin.io>
2026-06-02 20:36:09 +0000

    Fix postgres flake evaluation
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- flakes/postgres/flake.nix
+++ flakes/postgres/flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {nixpkgs}: let
+  outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = f:
       nixpkgs.lib.genAttrs systems (system:
