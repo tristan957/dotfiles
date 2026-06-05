81142c15e7bbe86780744142959611a7e37727fa
Tristan Partin <tristan@partin.io>
2026-06-05 20:29:38 +0000

    Add some keymaps for diffs.nvim
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/lua/plugins/diffs/init.lua
+++ modules/neovim/nvim/lua/plugins/diffs/init.lua
@@ -13,4 +13,13 @@ return {
       },
     }
   end,
+  config = function()
+    vim.keymap.set("n", "<leader>co", "<Plug>(diffs-conflict-ours)")
+    vim.keymap.set("n", "<leader>ct", "<Plug>(diffs-conflict-theirs)")
+    vim.keymap.set("n", "<leader>cb", "<Plug>(diffs-conflict-both)")
+    vim.keymap.set("n", "<leader>c0", "<Plug>(diffs-conflict-none)")
+    -- I use the keymaps from mini.bracketed
+    -- vim.keymap.set("n", "]x", "<Plug>(diffs-conflict-next)")
+    -- vim.keymap.set("n", "[x", "<Plug>(diffs-conflict-prev)")
+  end,
 }
