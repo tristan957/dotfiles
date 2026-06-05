91911f7a497c733ac57674eea2e95a57dc4640ac
Tristan Partin <tristan@partin.io>
2026-06-05 15:20:51 +0000

    Use 4-width tabstops for mail filetypes
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/after/ftplugin/mail.lua
+++ modules/neovim/nvim/after/ftplugin/mail.lua
@@ -3,4 +3,6 @@ vim.opt_local.expandtab = false
 vim.opt_local.fileformat = "dos"
 vim.opt_local.formatoptions = "1jnwtcql/r"
 vim.opt_local.modeline = false
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
 vim.opt_local.textwidth = 72
--- modules/vim/config/after/ftplugin/mail.vim
+++ modules/vim/config/after/ftplugin/mail.vim
@@ -3,4 +3,6 @@ setlocal fileformat=dos
 setlocal formatoptions=1jnwtcql/r
 setlocal noexpandtab
 setlocal nomodeline
+setlocal softtabstop = 4
+setlocal tabstop = 4
 setlocal textwidth=72
