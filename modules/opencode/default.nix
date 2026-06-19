3ad18e6db5e273c017e594f17fcac9cc6e60a9f8
Tristan Partin <tristan@partin.io>
2026-06-18 23:32:17 -0500

    Add fastmail MCP support to opencode
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/opencode/default.nix
+++ modules/opencode/default.nix
@@ -8,6 +8,18 @@
   config = lib.mkIf config.modules.opencode.enable {
     programs.opencode = {
       enable = true;
+
+      settings = {
+        mcp = {
+          fastmail = {
+            enabled = false;
+            type = "remote";
+            url = "https://api.fastmail.com/mcp";
+            oauth = {};
+          };
+        };
+      };
+
       skills = ../../skills;
       themes = ./themes;
       tui.theme = "one-dark-vivid";
