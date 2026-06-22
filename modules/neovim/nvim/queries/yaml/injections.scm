6f9923a4db107a88b7949dd9eedabfafe02de5b0
Tristan Partin <tristan@partin.io>
2026-06-22 22:16:23 +0000

    Clean up yaml injections
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/queries/yaml/injections.scm
+++ modules/neovim/nvim/queries/yaml/injections.scm
@@ -7,10 +7,11 @@
   (double_quote_scalar)
   (single_quote_scalar)
 ] @injection.content
+  (#filename-lua-match? "%.github/workflows/[^/]+%.ya?ml$")
   (#lua-match? @injection.content "[$]{{.*}}")
   (#set! injection.language "ghactions"))
 
-; Sourcehut build manifests
+; Sourcehut build manifests (.build.yaml)
 ((block_mapping_pair
     key: (flow_node
       (plain_scalar (string_scalar) @key))
@@ -22,7 +23,24 @@
               (block_mapping_pair
                 value: (block_node
                   (block_scalar) @injection.content)))))))
+  (#filename-lua-match? "%.build%.ya?ml$")
   (#eq? @key "tasks")
-  (#filename-lua-match? ".build.ya?ml")
+  (#set! injection.language "sh")
+))
+
+; Sourcehut build manifests (.builds/*.yaml)
+((block_mapping_pair
+    key: (flow_node
+      (plain_scalar (string_scalar) @key))
+    value: (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_mapping
+              (block_mapping_pair
+                value: (block_node
+                  (block_scalar) @injection.content)))))))
+  (#filename-lua-match? "%.builds/[^/]+%.ya?ml$")
+  (#eq? @key "tasks")
   (#set! injection.language "sh")
 ))
