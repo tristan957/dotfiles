679b47547742c0444bb82c38b5880ab85083a81c
Tristan Partin <tristan@partin.io>
2026-06-22 23:44:52 +0000

    Improve the tmux config more
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -6,8 +6,12 @@
   options.modules.tmux.enable = lib.mkEnableOption "tmux";
 
   config = lib.mkIf config.modules.tmux.enable {
-    home.file.".local/bin/fzf-tmux-sessions" = {
-      source = ./fzf-tmux-sessions;
+    home.file.".local/libexec/tmux/fzf-sessions" = {
+      source = ./fzf-sessions;
+      executable = true;
+    };
+    home.file.".local/libexec/tmux/session-input" = {
+      source = ./session-input;
       executable = true;
     };
     xdg.configFile."tmux/tmux.conf".source = ./tmux.conf;
