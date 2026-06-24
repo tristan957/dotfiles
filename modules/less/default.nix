6c767756678096e919823ae24e43d7e2fc1a7404
Tristan Partin <tristan@partin.io>
2026-06-24 21:01:35 +0000

    Migrate less config to more native home-manager
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/less/default.nix
+++ modules/less/default.nix
@@ -6,8 +6,17 @@
   options.modules.less.enable = lib.mkEnableOption "less";
 
   config = lib.mkIf config.modules.less.enable {
+    programs.less = {
+      enable = true;
+
+      options = {
+        RAW-CONTROL-CHARS = true;
+        no-histdups = true;
+        tabs = 4;
+      };
+    };
+
     home.sessionVariables = {
-      LESS = "Rx4";
       LESSHISTSIZE = 1000000;
       LESSOPEN = "| pygmentize -O style=one-dark %s 2>/dev/null";
     };
