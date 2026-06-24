59aefd3f9532b2ac73ba457d496fb79c67f20df6
Tristan Partin <tristan@partin.io>
2026-06-24 16:38:20 +0000

    Expand nixd settings
    Set the formatter to alejandra and add the nixpkgs expression.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/after/lsp/nixd.lua
+++ modules/neovim/nvim/after/lsp/nixd.lua
@@ -1,4 +1,14 @@
 ---@type vim.lsp.Config
 return {
   cmd = { "nixd", "--inlay-hints", "--semantic-tokens" },
+  settings = {
+    nixd = {
+      formatting = {
+        command = { "alejandra" },
+      },
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+    },
+  },
 }
