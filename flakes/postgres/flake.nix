7ebba6ccf978e21b30db91632f74a9f66a336774
Tristan Partin <tristan@partin.io>
2026-06-25 17:36:04 -0500

    Migrate elfutils and gcc16 to linux only in the Postgres flake
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- flakes/postgres/flake.nix
+++ flakes/postgres/flake.nix
@@ -1,122 +1,129 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    nixpkgs,
+  outputs = inputs @ {
+    flake-parts,
     systems,
     ...
-  }: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    devShells = forEachSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      default = pkgs.mkShell {
-        # _FORTIFY_SOURCE requires -O1+, conflicts with -O0 debug builds
-        hardeningDisable = ["fortify"];
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
 
-        # Nix's ICU package sets libdir in its .pc files to the -dev output, which
-        # contains no .so files. Meson trusts that path, fails to find libicuuc.so,
-        # and falls back to the system ICU (wrong version). Fix by providing .pc
-        # files with libdir pointing to the actual library output.
-        shellHook = ''
-          export PKG_CONFIG_PATH="${
-            pkgs.runCommand "icu-pc-fix" {} ''
-              mkdir -p $out/lib/pkgconfig
-              for pc in ${pkgs.icu.dev}/lib/pkgconfig/*.pc; do
-                sed "s|libdir *=.*|libdir=${pkgs.icu}/lib|" "$pc" > "$out/lib/pkgconfig/$(basename "$pc")"
-              done
-            ''
-          }/lib/pkgconfig''${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
-        '';
+      perSystem = {
+        lib,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          # _FORTIFY_SOURCE requires -O1+, conflicts with -O0 debug builds
+          hardeningDisable = ["fortify"];
 
-        packages = with pkgs;
-          [
-            # Build dependencies
-            autoconf269
-            bison
-            clang
-            flex
-            gcc16
-            meson
-            muon
-            ninja
-            perl
-            pkgconf
-            python314
-            samurai
+          # Nix's ICU package sets libdir in its .pc files to the -dev output, which
+          # contains no .so files. Meson trusts that path, fails to find libicuuc.so,
+          # and falls back to the system ICU (wrong version). Fix by providing .pc
+          # files with libdir pointing to the actual library output.
+          shellHook = ''
+            export PKG_CONFIG_PATH="${
+              pkgs.runCommand "icu-pc-fix" {} ''
+                mkdir -p $out/lib/pkgconfig
+                for pc in ${pkgs.icu.dev}/lib/pkgconfig/*.pc; do
+                  sed "s|libdir *=.*|libdir=${pkgs.icu}/lib|" "$pc" > "$out/lib/pkgconfig/$(basename "$pc")"
+                done
+              ''
+            }/lib/pkgconfig''${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
+          '';
 
-            # Libraries
-            # Postgres does not support aws-lc
-            # aws-lc
-            curl
-            gettext
-            icu
-            krb5
-            libedit
-            libressl
-            libxml2
-            libxslt
-            llvm
-            lz4
-            openldap
-            openssl
-            readline
-            tcl
-            libuuid
-            zlib
-            zstd
+          packages = with pkgs;
+            [
+              # Build dependencies
+              autoconf269
+              bison
+              clang
+              flex
+              meson
+              muon
+              ninja
+              perl
+              pkgconf
+              python314
+              samurai
 
-            # Docs
-            ditaa
-            docbook2x
-            docbook_xml_dtd_45
-            docbook_xsl
-            fop
-            graphviz
-            pandoc
+              # Libraries
+              # Postgres does not support aws-lc
+              # aws-lc
+              curl
+              gettext
+              icu
+              krb5
+              libedit
+              libressl
+              libxml2
+              libxslt
+              llvm
+              lz4
+              openldap
+              openssl
+              readline
+              tcl
+              libuuid
+              zlib
+              zstd
 
-            # Testing
-            perlPackages.IPCRun
-            python314Packages.pytest
+              # Docs
+              ditaa
+              docbook2x
+              docbook_xml_dtd_45
+              docbook_xsl
+              fop
+              graphviz
+              pandoc
 
-            # Dev tools
-            bash-language-server
-            bear
-            binutils
-            ccache
-            clang-tools
-            cppcheck
-            elfutils
-            git
-            include-what-you-use
-            lcov
-            lldb
-            pgformatter
-            postgres-language-server
-            shellcheck
-          ]
-          ++ lib.optionals stdenv.isLinux [
-            avahi-compat
-            gdb
-            libselinux
-            libsepol
-            liburing
-            linux-pam
-            linuxPackages.systemtap
-            ltrace
-            numactl
-            pcre2
-            perf
-            rr
-            strace
-            systemdLibs
-            valgrind
-          ];
+              # Testing
+              perlPackages.IPCRun
+              python314Packages.pytest
+
+              # Dev tools
+              bash-language-server
+              bear
+              binutils
+              ccache
+              clang-tools
+              cppcheck
+              git
+              include-what-you-use
+              lcov
+              lldb
+              pgformatter
+              postgres-language-server
+              shellcheck
+            ]
+            ++ lib.optionals stdenv.isLinux [
+              avahi-compat
+              elfutils
+              gcc16
+              gdb
+              libselinux
+              libsepol
+              liburing
+              linux-pam
+              linuxPackages.systemtap
+              ltrace
+              numactl
+              pcre2
+              perf
+              rr
+              strace
+              systemdLibs
+              valgrind
+            ];
+        };
       };
-    });
-  };
+    };
 }
