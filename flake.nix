908e3d8dfded5e37001592fbf0dae152ae9d95de
Tristan Partin <tristan@partin.io>
2026-06-25 14:50:31 -0500

    Migrate the flake to use flake-parts
    Presumably this will make things better in the future as my nix skills
    continue to grow.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,50 +14,53 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    nixpkgs,
+  outputs = inputs @ {
+    flake-parts,
     home-manager,
+    nixpkgs,
     systems,
     ...
-  }: let
-    mkHome = import ./lib/mk-home.nix {inherit nixpkgs home-manager;};
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    inherit (import ./machines/common.nix) commonModules emailModules rustModules;
-  in {
-    devShells = forEachSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: let
+      mkHome = import ./lib/mk-home.nix {inherit nixpkgs home-manager;};
+      inherit (import ./machines/common.nix) commonModules emailModules rustModules;
     in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          alejandra
-          bashInteractive
-          fish
-          lua-language-server
-          luajitPackages.luacheck
-          markdownlint-cli2
-          nixd
-          prettier
-          reuse
-          ruff
-          shellcheck
-          shfmt
-          stylua
-          ty
-          vscode-langservers-extracted
-          zsh
-        ];
+      systems = import systems;
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            alejandra
+            bashInteractive
+            fish
+            lua-language-server
+            luajitPackages.luacheck
+            markdownlint-cli2
+            nixd
+            prettier
+            reuse
+            ruff
+            shellcheck
+            shfmt
+            stylua
+            ty
+            vscode-langservers-extracted
+            zsh
+          ];
+        };
+      };
+
+      flake = {
+        homeConfigurations = let
+          machineArgs = {inherit commonModules emailModules rustModules;};
+        in {
+          "dbltap@macbook" = mkHome (import ./machines/macbook.nix machineArgs);
+          "dbltap@dbltap-dev" = mkHome (import ./machines/dbltap-dev.nix machineArgs);
+          "dbltap@dbltap-lts" = mkHome (import ./machines/dbltap-lts.nix machineArgs);
+          "tristan957@c-3po" = mkHome (import ./machines/c-3po.nix machineArgs);
+        };
+
+        homeModules.default = ./home.nix;
       };
     });
-
-    homeManagerModules.default = ./home.nix;
-
-    homeConfigurations = let
-      machineArgs = {inherit commonModules emailModules rustModules;};
-    in {
-      "dbltap@macbook" = mkHome (import ./machines/macbook.nix machineArgs);
-      "dbltap@dbltap-dev" = mkHome (import ./machines/dbltap-dev.nix machineArgs);
-      "dbltap@dbltap-lts" = mkHome (import ./machines/dbltap-lts.nix machineArgs);
-      "tristan957@c-3po" = mkHome (import ./machines/c-3po.nix machineArgs);
-    };
-  };
 }
