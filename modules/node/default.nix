9b7882b1b13134bcbe6237f50e7642448e33127c
Tristan Partin <tristan@partin.io>
2026-06-25 22:05:29 -0500

    Pre-create the node state directory
    Node will not create it by itself.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/node/default.nix
+++ modules/node/default.nix
@@ -1,7 +1,15 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   config = {
     home.sessionVariables = {
       NODE_REPL_HISTORY = "${config.xdg.stateHome}/node/history";
     };
+
+    home.activation.createNodeStateDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir -p "${config.xdg.stateHome}/node"
+    '';
   };
 }
