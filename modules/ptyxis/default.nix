ad5bfef746ca94344db74003c5460a0ad11ea8d9
Tristan Partin <tristan@partin.io>
2026-06-26 00:32:14 -0500

    Move ptyixs palette generation into home-manager
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/ptyxis/default.nix
+++ modules/ptyxis/default.nix
@@ -4,6 +4,70 @@
   ...
 }: {
   config = lib.mkIf pkgs.stdenv.isLinux {
-    xdg.dataFile."app.devsuite.Ptyxis/palettes".source = ./palettes;
+    programs.ptyxis.enable = true;
+
+    programs.ptyxis.palettes = {
+      One-Vivid = {
+        Palette.Name = "One Vivid";
+        Dark = {
+          Background = "#282c34";
+          # Foreground = #abb2bf;
+          Foreground = "#ffffff";
+
+          Color0 = "#282c34";
+          Color8 = "#5c6370";
+
+          Color1 = "#ef596f";
+          Color9 = "#f38897";
+
+          Color2 = "#89ca78";
+          Color10 = "#a9d89d";
+
+          Color3 = "#e5c07b";
+          Color11 = "#edd4a6";
+
+          Color4 = "#61afef";
+          Color12 = "#8fc6f4";
+
+          Color5 = "#d55fde";
+          Color13 = "#e089e7";
+
+          Color6 = "#2bbac5";
+          Color14 = "#4bced8";
+
+          Color7 = "#abb2bf";
+          Color15 = "#c8cdd5";
+        };
+        Light = {
+          Background = "#fafafa";
+          # Foreground = #6a6a6a
+          Foreground = "#000000";
+
+          Color0 = "#6a6a6a";
+          Color8 = "#bebebe";
+
+          Color1 = "#e05661";
+          Color9 = "#e88189";
+
+          Color2 = "#1da912";
+          Color10 = "#25d717";
+
+          Color3 = "#eea825";
+          Color11 = "#f2bb54";
+
+          Color4 = "#118dc3";
+          Color12 = "#1caceb";
+
+          Color5 = "#9a77cf";
+          Color13 = "#b69ddc";
+
+          Color6 = "#56b6c2";
+          Color14 = "#7bc6d0";
+
+          Color7 = "#fafafa";
+          Color15 = "#ffffff";
+        };
+      };
+    };
   };
 }
