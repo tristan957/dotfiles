23db5118b2fe13a8751a30f986c09fd62b3f4d6a
Tristan Partin <tristan@partin.io>
2026-06-26 16:04:14 -0500

    Remove filetype lazy loading for tree-sitter-ghostty
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/lua/plugins/ghostty/init.lua
+++ modules/neovim/nvim/lua/plugins/ghostty/init.lua
@@ -3,7 +3,6 @@
 ---@type LazySpec
 return {
   "bezhermoso/tree-sitter-ghostty",
-  ft = "ghostty",
   build = "make nvim_install",
   cond = require("tristan957.treesitter").is_cli_available() and vim.fn.executable("make") == 1,
 }
