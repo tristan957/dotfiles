0f1dde33c3fa0d691b5a6ecbe8740e74bee07045
Tristan Partin <tristan@partin.io>
2026-06-25 20:35:13 -0500

    Move cargo config into home-manager
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/cargo/default.nix
+++ modules/cargo/default.nix
@@ -1,14 +1,34 @@
 {config, ...}: {
   config = {
-    home.sessionPath = [
-      "$HOME/.opt/cargo/bin"
-    ];
+    programs.cargo = {
+      enable = true;
 
-    home.sessionVariables = {
-      CARGO_HOME = "${config.home.homeDirectory}/.opt/cargo";
-      CARGO_INSTALL_ROOT = "${config.home.homeDirectory}/.local";
+      # Keep package null so cargo continues to come from rustup.
+      package = null;
+
+      cargoHome = "${config.home.homeDirectory}/.opt/cargo";
+
+      settings = {
+        install.root = "${config.home.homeDirectory}/.local";
+
+        net = {
+          # Given a more complex Git configuration, the Cargo Git library is
+          # pretty naive. It doesn't resolve the config entirely. Use the Git
+          # CLI to avoid any Git config problems.
+          git-fetch-with-cli = true;
+        };
+
+        target = {
+          stable-aarch64-apple-darwin = {
+            linker = "clang";
+            rustflags = ["-C" "link-arg=-fuse-ld=mold"];
+          };
+          x86_64-unknown-linux-gnu = {
+            linker = "clang";
+            rustflags = ["-C" "link-arg=-fuse-ld=mold"];
+          };
+        };
+      };
     };
-
-    home.file.".opt/cargo/config.toml".source = ./config.toml;
   };
 }
