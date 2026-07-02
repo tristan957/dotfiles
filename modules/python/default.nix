81776a73ffadb1188af82231c20d86c8317b4ff8
Tristan Partin <tristan@partin.io>
2026-07-02 20:58:51 +0000

    Create Python state directory
    Otherwise Python will complain about not being able to write to the
    history file.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -1,9 +1,17 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   config = {
     home.sessionVariables = {
       PYTHON_HISTORY = "${config.xdg.stateHome}/python/history";
       # Python virtual environments should stop messing with PS1
       VIRTUAL_ENV_DISABLE_PROMPT = 1;
     };
+
+    home.activation.createPythonStateDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      mkdir -p "${config.xdg.stateHome}/python"
+    '';
   };
 }
