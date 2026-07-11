249a1b84ab14a05fb3f0c066ad31fd2378228cde
Tristan Partin <tristan@partin.io>
2026-07-10 22:53:59 -0700

    Remove neovim theme hack
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/plugin/theme.lua
+++ modules/neovim/nvim/plugin/theme.lua
@@ -1,5 +1,3 @@
-local system = require("tristan957.utils.system")
-
 local DARK_THEME = "onedark_vivid"
 local LIGHT_THEME = "onelight"
 
@@ -10,7 +8,7 @@ local function set_theme(theme)
   vim.cmd.colorscheme(theme == "light" and LIGHT_THEME or DARK_THEME)
 end
 
-set_theme(system.color_scheme())
+set_theme(vim.o.background)
 
 vim.api.nvim_create_autocmd("OptionSet", {
   pattern = "background",
