193f0d569093ab25952ae698f0b703928eb195a9
Tristan Partin <tristan@partin.io>
2026-07-10 23:28:58 -0700

    Inline mini.misc opts
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/lua/plugins/mini-misc/init.lua
+++ modules/neovim/nvim/lua/plugins/mini-misc/init.lua
@@ -3,14 +3,13 @@
 ---@type LazySpec
 return {
   "nvim-mini/mini.misc",
-  opts = {
-    center = true,
-  },
   config = function()
     local MiniMisc = require("mini.misc")
 
     MiniMisc.setup()
 
-    MiniMisc.setup_restore_cursor()
+    MiniMisc.setup_restore_cursor({
+      center = true,
+    })
   end,
 }
