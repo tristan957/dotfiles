e57377801c88b194811bdd3de6717929d365e7bd
Tristan Partin <tristan@partin.io>
2026-07-13 10:29:07 -0500

    Set up the claude-code-DO-NOT-DELETE profile
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- flakes/work/modules/ada/default.nix
+++ flakes/work/modules/ada/default.nix
@@ -8,8 +8,17 @@
         Provider = "isengard";
         Region = "us-west-2";
       };
-      profiles.dbltap = {
-        Account = "534344665149";
+      profiles = {
+        # To effectively use this account for things like OpenCode, I need to
+        # set up user-agent strings per model. I'll just continue to use Bedrock
+        # through my personal Isengard account.
+        claude-code-DO-NOT-DELETE = {
+          Account = "175342148895";
+          Role = "CeceliaAmazonInternal";
+        };
+        dbltap = {
+          Account = "534344665149";
+        };
       };
     };
   };
