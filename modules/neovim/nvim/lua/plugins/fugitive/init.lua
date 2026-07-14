c12237a30065353dceca303790c5fea4bf847396
Tristan Partin <tristan@partin.io>
2026-07-13 22:57:42 -0500

    Add buffer specfic keymap to yank Git SHA in fugitive buffers
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/lua/plugins/fugitive/init.lua
+++ modules/neovim/nvim/lua/plugins/fugitive/init.lua
@@ -4,4 +4,19 @@
 return {
   "tpope/vim-fugitive",
   cmd = { "Git" },
+  config = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "FugitiveCommit",
+      desc = "Set up commit object buffer keymaps",
+      callback = function(args)
+        vim.keymap.set("n", "ys", function()
+          local hash = vim.fn.FugitiveParse()[1]
+          local register = vim.v.register
+
+          vim.fn.setreg(register, hash)
+          vim.notify(string.format('Yanked commit hash into register "%s: %s', register, hash))
+        end, { buffer = args.buf, desc = "Yank the current commit's hash" })
+      end,
+    })
+  end,
 }
