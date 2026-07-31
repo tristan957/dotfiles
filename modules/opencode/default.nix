6c6a6ebc73719e971d9da0e3304a1b72b4f25533
Tristan Partin <tristan@partin.io>
2026-07-31 00:52:58 -0500

    Enable LSP in opencode
    Keep opencode from downloading language servers.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/opencode/default.nix
+++ modules/opencode/default.nix
@@ -10,7 +10,19 @@ in {
     programs.opencode = {
       enable = true;
 
+      package = pkgs.symlinkJoin {
+        inherit (pkgs.opencode) meta;
+        name = "opencode-wrapped-${pkgs.opencode.version}";
+        paths = [pkgs.opencode];
+        nativeBuildInputs = [pkgs.makeWrapper];
+        postBuild = ''
+          wrapProgram $out/bin/opencode \
+            --set OPENCODE_DISABLE_LSP_DOWNLOAD true
+        '';
+      };
+
       settings = {
+        lsp = {};
         mcp = mcp.opencode.generate [
           mcp.servers._1password
           mcp.servers.fastmail
