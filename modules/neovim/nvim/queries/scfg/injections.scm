afecf9ee3528b1e21b9e1738009959c73ab3f04c
Tristan Partin <tristan@partin.io>
2026-07-31 08:12:21 +0000

    Fix mjmap scfg injection
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/queries/scfg/injections.scm
+++ modules/neovim/nvim/queries/scfg/injections.scm
@@ -7,6 +7,6 @@
     (word
       (dquote_word) @injection.content)))
  (#eq? @_name "token-cmd")
- (#filename-lua-match? ".*config/mjmap/config%.scfg$")
+ (#filename-lua-match? ".*mjmap/config%.scfg$")
  (#set! injection.language "sh")
  (#offset! @injection.content 0 1 0 -1)) ; Exclude the quotes
