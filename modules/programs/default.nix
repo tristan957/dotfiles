421b4663cc18366883148e964f448c8f4960152c
Tristan Partin <tristan@partin.io>
2026-07-31 01:16:16 -0500

    Remove xdp-file-chooser
    I never used it.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -9,7 +9,6 @@
       "${config.xdg.binHome}/pgsql-hacker" = config.lib.file.mkExecutable ./pgsql-hacker;
       "${config.xdg.binHome}/substr" = config.lib.file.mkExecutable ./substr;
       "${config.xdg.binHome}/universal-copy" = config.lib.file.mkExecutable ./universal-copy;
-      "${config.xdg.binHome}/xdp-file-chooser" = config.lib.file.mkExecutable ./xdp-file-chooser;
       "${config.xdg.binHome}/yaml2json" = config.lib.file.mkExecutable ./yaml2json;
     };
   };
