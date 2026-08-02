25e1ceb8b055156d76780675642df28e5def7d89
Tristan Partin <tristan@partin.io>
2026-08-02 00:06:58 +0000

    Add gitsendemail filetype settings
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/after/ftplugin/gitsendemail.lua
+++ modules/neovim/nvim/after/ftplugin/gitsendemail.lua
@@ -1,1 +1,8 @@
+vim.opt_local.colorcolumn = { "72" }
+vim.opt_local.expandtab = false
+vim.opt_local.fileformat = "dos"
+vim.opt_local.formatoptions = "1jnwtcqlr"
+vim.opt_local.modeline = false
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
 vim.opt_local.textwidth = 72
