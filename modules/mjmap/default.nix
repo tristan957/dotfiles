ab215c38d8e00c2f190f7ab4023a5116ef41c0ad
Tristan Partin <tristan@partin.io>
2026-08-04 01:19:15 -0500

    Set mjmap config in $HOME/Library/Application Support on macOS
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/mjmap/default.nix
+++ modules/mjmap/default.nix
@@ -1,5 +1,15 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   config = {
     xdg.configFile."mjmap/config.scfg".source = ./config.scfg;
+
+    # I have fixed upstream mjmap to look at XDG_CONFIG_HOME on macOS, but
+    # this has not made its way to a release yet
+    home.file = lib.optionalAttrs pkgs.stdenv.isDarwin {
+      "Library/Application Support/mjmap/config.scfg".source = ./config.scfg;
+    };
   };
 }
