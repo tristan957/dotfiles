9bf2cd8bd4020c557aea46361636a9669a1c3296
Tristan Partin <tristan@partin.io>
2026-08-11 23:01:09 -0500

    Keep less history under XDG state
    The trailing semicolon on LESSHISTSIZE became part of the value, since
    lesskey #env entries take the rest of the line. LESSHISTFILE was also
    unset, so less wrote ~/.lesshst instead of following the XDG layout used
    everywhere else.

--- modules/less/default.nix
+++ modules/less/default.nix
@@ -1,14 +1,28 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   config = {
     home.sessionVariables.PAGER = "less";
+
+    home.activation.createLessStateDir =
+      lib.hm.dag.entryAfter ["writeBoundary"]
+      # bash
+      ''
+        mkdir -p "${config.xdg.stateHome}/less"
+      '';
 
     programs.less = {
       enable = true;
 
-      config = ''
-        #env
-        LESSHISTSIZE = 1000000;
-      '';
+      config =
+        # lesskey
+        ''
+          #env
+          LESSHISTFILE = ${config.xdg.stateHome}/less/history
+          LESSHISTSIZE = 1000000
+        '';
 
       options = {
         RAW-CONTROL-CHARS = true;
