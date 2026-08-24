32e67f357e3a63e73311634a09db0cb36b2141f4
Tristan Partin <tristan@partin.io>
2026-08-24 14:31:07 -0500

    Disable vim-sleuth in gitsendemail and mail filetypes
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/lua/plugins/sleuth/init.lua
+++ modules/neovim/nvim/lua/plugins/sleuth/init.lua
@@ -6,5 +6,7 @@ return {
   event = { "BufNewFile", "BufReadPre" },
   init = function()
     vim.g.sleuth_gitcommit_heurisitics = 0
+    vim.g.sleuth_gitsendemail_heurisitics = 0
+    vim.g.sleuth_mail_heurisitics = 0
   end,
 }
