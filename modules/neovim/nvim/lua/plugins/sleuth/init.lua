2d3329f07358111c5372dd4a30f0ee5dee5d0ebb
Tristan Partin <tristan@partin.io>
2026-08-26 21:55:23 -0500

    Fix typo in sleuth plugin config
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/neovim/nvim/lua/plugins/sleuth/init.lua
+++ modules/neovim/nvim/lua/plugins/sleuth/init.lua
@@ -5,8 +5,8 @@ return {
   "tpope/vim-sleuth",
   event = { "BufNewFile", "BufReadPre" },
   init = function()
-    vim.g.sleuth_gitcommit_heurisitics = 0
-    vim.g.sleuth_gitsendemail_heurisitics = 0
-    vim.g.sleuth_mail_heurisitics = 0
+    vim.g.sleuth_gitcommit_heuristics = 0
+    vim.g.sleuth_gitsendemail_heuristics = 0
+    vim.g.sleuth_mail_heuristics = 0
   end,
 }
