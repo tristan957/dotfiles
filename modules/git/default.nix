9258187884f6c044a62d333a4d8e430cc0361c19
Tristan Partin <tristan@partin.io>
2026-09-22 12:30:39 -0500

    Remove the interactive-diff script
    It is a pointless indication now that all machines get delta via
    nixpkgs.
    
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -6,7 +6,6 @@
       "${config.xdg.binHome}/git-credential-bw" = config.lib.file.mkExecutable ./git-credential-bw;
       "${config.xdg.binHome}/git-credential-op" = config.lib.file.mkExecutable ./git-credential-op;
       "${config.xdg.binHome}/git-user" = config.lib.file.mkExecutable ./git-user;
-      "${config.xdg.binHome}/interactive-diff" = config.lib.file.mkExecutable ./interactive-diff;
     };
   };
 }
